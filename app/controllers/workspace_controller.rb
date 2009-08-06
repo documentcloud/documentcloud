e5e6569b17369a5dd13dea48cad5c4a7de51b61d
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-06 11:33:08 -0400

    hello documentcloud

---
+++ app/controllers/workspace_controller.rb
@@ -0,0 +1,7 @@
+class WorkspaceController < ApplicationController
+  
+  layout 'barebones'
+  
+  def index;  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,8 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  
+  map.root :controller => "workspace"
 
   # See how all your routes lay out with "rake routes"
 
