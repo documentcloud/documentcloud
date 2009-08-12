14dd945c5e321918226a8f44d4c1fa023b521f47
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-12 12:46:57 -0400

    adding a protoype hash ring implementation that we can flesh out -- keep it in mind

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
+  require 'digest/md5'
+  
   config.gem 'json', :version => '>= 1.1.7'
   config.gem 'faker', :version => '>= 0.3.1'
   config.gem 'calais', :version => '>= 0.0.7'
