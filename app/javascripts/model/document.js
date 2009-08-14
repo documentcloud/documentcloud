991eecae3baad6915dad733ac5d552e1ae96a7e3
Jeremy Ashkenas <jashkenas@gmail.com>
2009-08-14 13:48:10 -0400

    adding a method to gather a document's metadata

--- app/javascripts/model/document.js
+++ app/javascripts/model/document.js
@@ -1,5 +1,16 @@
 dc.model.Document = dc.Model.extend({
   
+  // Return a list of the document's metadata. Think about caching this on the
+  // document by binding to Metadata, instead of on-the-fly.
+  metadata : function() {
+    var docId = this.id;
+    return _.select(Metadata.values(), function(m) {
+      return _.any(m.get('instances'), function(i){ 
+        return i.document_id == docId; 
+      });
+    });
+  },
+  
   toString : function() {
     return 'Document ' + this.id + ' "' + this.get('title') + '"';
   }
