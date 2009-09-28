5fe587ad178c2c5176c45fde7b9ce398b696a016
Jeremy Ashkenas <jashkenas@gmail.com>
2009-09-28 16:16:13 -0400

    got frontpage searching working -- going into workspace

---
+++ app/javascripts/app/workspace.js
@@ -0,0 +1,78 @@
+// Main controller for the workspace.
+dc.app.workspace = {
+  
+  // On Document ready, bind all callbacks.
+  initialize : function() {
+  
+    $(document).ready(function() {
+      // Move this somewhere. Prevent '#' links from page jumping.
+      $('body').bind('click', function(e){
+        if (e.target.tagName.toUpperCase() == 'A' && e.target.href == window.location.toString() + '#') {
+          e.preventDefault();
+        }
+      });
+    
+      $('#workspace_document_list').html((new dc.ui.DocumentList()).render().el);
+    
+      var el = $('#search');
+      el.bind('keydown', function(e) {
+        if (!el.outstandingSearch && e.keyCode == 13 && el.val()) {
+          dc.app.workspace.search(el.val());
+        }
+      });
+      
+      $('#wordmark').bind('click', function(){ window.location = '/'; });
+      
+      dc.app.workspace.performDefaultSearch();
+    });
+    
+  },
+  
+  search : function(query) {
+    var el = $('#search');
+    el.outstandingSearch = true;
+    dc.ui.Spinner.show('searching');
+    $('.documents').html('');
+    $('#metadata').html('');
+    $.get('/search.json', {query_string : query}, function(resp) {        
+      if (window.console) console.log(resp);
+
+      Documents.refresh(_.map(resp.documents, function(m){ return new dc.model.Document(m); }));
+
+      var query = new dc.ui.Query(resp.query).render(resp.documents.length);
+      $('#query_container').html(query.el);
+
+      _.each(Documents.values(), function(el) {
+        $('.documents').append((new dc.ui.DocumentTile(el)).render().el);
+      });
+
+      if (resp.documents.length == 0) {
+        dc.ui.Spinner.hide();
+        el.outstandingSearch = false;
+        return;
+      }
+
+      dc.ui.Spinner.show('gathering metadata');
+      $.get('/documents/metadata.json', {'ids[]' : _.pluck(resp.documents, 'id')}, function(resp2) {
+
+        Metadata.refresh();
+        _.each(resp2.metadata, function(m){ Metadata.addOrCreate(m); });
+        Metadata.sort();
+        var mView = new dc.ui.MetadataList({metadata : Metadata.values()});
+        $('#metadata').html(mView.render().el);
+
+        dc.ui.Spinner.hide();
+        el.outstandingSearch = false;
+      }, 'json');
+    }, 'json');
+  },
+  
+  performDefaultSearch : function() {
+    var query = window.SEARCH_QUERY;
+    if (query) {
+      $('#search').val(query);
+      dc.app.workspace.search(query);
+    }
+  }
+  
+};
