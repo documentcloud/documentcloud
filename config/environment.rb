3362d446fbe01a4462b8f43f277a89bb2aa79d10
Jeremy Ashkenas <jashkenas@gmail.com>
2009-09-29 14:29:00 -0400

    changing to rails 2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ end
 end
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
