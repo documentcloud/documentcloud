8aa4a26e95b05132084d114f789d3548cd449f69
Jeremy Ashkenas <jashkenas@gmail.com>
2009-09-30 08:31:13 -0400

    buttons and port 8080 for cloudcrowd

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -2,7 +2,10 @@ class WorkspaceController < ApplicationController
   
   before_filter :authenticate unless Rails.development?
     
-  def index;  end
+  def index;
+    # temporary -- until we have real accounts.
+    @account = !!params[:account]
+  end
   
   def signup; end
   
