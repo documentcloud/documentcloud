af059d2338e3fca28fcf7fa09438b9354f49ff16
Jeremy Ashkenas <jashkenas@gmail.com>
2009-09-30 22:31:34 -0400

    moving continuation of the cloudcrowd job into a deferred thread

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -15,24 +15,24 @@ class ImportController < ApplicationController
   def cloud_crowd
     job = JSON.parse(params[:job])
     raise "CloudCrowd processing failed" if job['status'] != 'succeeded'
-    job['outputs'].each do |result|
-      logger.info "Importing #{File.basename(result['pdf_url'])}"
-      doc = Document.new({
-        :title                => result['title'],
-        :organization         => result['source'],
-        :pdf_path             => result['pdf_url'],
-        :full_text            => fetch_contents(result['full_text_url']),
-        :rdf                  => fetch_contents(result['rdf_url']),
-        :thumbnail_path       => result['thumbnail_url'],
-        :small_thumbnail_path => result['small_thumbnail_url']
-      })
-      DC::Import::MetadataExtractor.new.extract_metadata(doc)
-      doc.save
-    end
     
-    # Cleaning up later so that we don't deadlock in development.
+    # Finishing the job later so that we don't deadlock in development.
     Thread.new do
       sleep 1
+      job['outputs'].each do |result|
+        logger.info "Importing #{File.basename(result['pdf_url'])}"
+        doc = Document.new({
+          :title                => result['title'],
+          :organization         => result['source'],
+          :pdf_path             => result['pdf_url'],
+          :full_text            => fetch_contents(result['full_text_url']),
+          :rdf                  => fetch_contents(result['rdf_url']),
+          :thumbnail_path       => result['thumbnail_url'],
+          :small_thumbnail_path => result['small_thumbnail_url']
+        })
+        DC::Import::MetadataExtractor.new.extract_metadata(doc)
+        doc.save
+      end
       RestClient.delete DC_CONFIG['cloud_crowd_server'] + "/jobs/#{job['id']}"
     end
     
