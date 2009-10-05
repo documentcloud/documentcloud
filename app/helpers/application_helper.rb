ee3b26fce0b0167db899bd1411515f78f5e1be74
Jeremy Ashkenas <jashkenas@gmail.com>
2009-10-05 14:32:59 -0400

    finishing up asset packages + extra resource integration -- trying it on staging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  def stylesheet_asset_tag
-    if Rails.development?
-      DC::Asset::Merger::CSS_URLS.map {|u| stylesheet_link_tag(u) }.join("\n")
-    else
-      stylesheet_link_tag '/assets/stylesheets.css'
-    end
+  def stylesheet_asset_tag(*extras)
+    base_tags = stylesheet_link_tag(
+      Rails.development? ? DC::Asset::Merger::CSS_URLS : '/assets/stylesheets.css')
+    base_tags << stylesheet_link_tag(extras)
   end
   
-  def javascript_asset_tag
-    if Rails.development?
-      DC::Asset::Merger::JS_URLS.map {|u| javascript_include_tag(u) }.join("\n")
-    else
-      javascript_include_tag '/assets/javascripts.js'
-    end
+  def javascript_asset_tag(*extras)
+    base_tags = javascript_include_tag(
+      Rails.development? ? DC::Asset::Merger::JS_URLS : '/assets/javascripts.js')
+    base_tags << javascript_include_tag(extras)
   end
   
   def jst_asset_tag
