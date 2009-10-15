2102de05e643d25710b135966b20fd7997805e90
Jeremy Ashkenas <jashkenas@gmail.com>
2009-10-15 17:45:35 -0400

    ooips

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,6 +8,7 @@ class SearchController < ApplicationController
   
   # Run a unified full-text/fielded search.
   def search
+    opts        = {}
     opts        = {:account_id => current_account.id, :organization_id => current_organization.id} if logged_in?
     query       = DC::Search::Parser.new.parse(params[:query_string])
     query.page  = params[:page] ? params[:page].to_i - 1 : 0
