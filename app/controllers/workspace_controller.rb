43652333016ed823ae7312a5ed61b1151353b954
Jeremy Ashkenas <jashkenas@gmail.com>
2009-10-20 21:46:19 -0400

    commenting out the workspace controller -- it's getting a little interesting

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -3,11 +3,16 @@ class WorkspaceController < ApplicationController
   RESULTS_REQUEST = /\A\/results/
   
   before_filter :bouncer unless Rails.development?
-    
-  def index    
-    redirect_to '/search' unless current_account || results_request?
+  
+  # Main documentcloud.org page. Renders the workspace if logged in or 
+  # searching, the home page otherwise.
+  def index
+    return if current_account || results_request?
+    render :template => 'search/search', :layout => 'empty'
   end
   
+  # Attempt a new signup for DocumentCloud -- includes both the organization and
+  # its first account. If everthing's kosher, the journalist is logged in.
   def signup
     return render unless request.post?
     org = Organization.create(params[:organization])
@@ -18,17 +23,21 @@ class WorkspaceController < ApplicationController
     redirect_to '/'
   end
   
+  # /login handles both the login form and the login request.
   def login
     return render unless request.post?
     account = Account.log_in(params[:email], params[:password], session)
     account ? redirect_to('/') : fail(true)
   end
   
+  # Logging out clears your entire session.
   def logout
     reset_session
     redirect_to '/'
   end
   
+  # Render the TODO.txt file out in public.
+  # TODO: (ha!) remove when no longer appropriate.
   def todo
     @todo_text = File.read("#{RAILS_ROOT}/TODO")
     @todo_text.gsub!(/^([A-Z]+:)/, '</ul><h2>\1</h2><ul>').gsub!(/\*(.+?)\n\s*\n/m, '<li>\1</li>')
