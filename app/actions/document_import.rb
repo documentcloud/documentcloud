2b8ce6f3e030d032cb622d158f722a52217c653a
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-08 17:13:23 -0500

    --

--- app/actions/document_import.rb
+++ app/actions/document_import.rb
@@ -4,6 +4,7 @@ RAILS_ENV = ENV['RAILS_ENV'] = ENV['RACK_ENV']
 
 # Load the DocumentCloud environment if we're in a Node context.
 if CloudCrowd.node?
+  require 'logger'
   Object.const_set "RAILS_DEFAULT_LOGGER", Logger.new(STDOUT)
   require 'rubygems'
   require 'activerecord'
