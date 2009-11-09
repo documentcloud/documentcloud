0d2a89b06c42f44d8d6b91bc465ff48b90b52950
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-08 22:26:39 -0500

    raising the update interval to 5 seconds

--- public/javascripts/model/processing_jobs.js
+++ public/javascripts/model/processing_jobs.js
@@ -1,22 +1,22 @@
 // ProcessingJob Model
 
 dc.model.ProcessingJob = dc.Model.extend({
-  
+
   DOCUMENT_STATUSES : {
-    splitting  : "starting...", 
+    splitting  : "starting...",
     processing : "processing pages...",
     merging    : "extracting entities..."
   },
-  
+
   constructor : function(options) {
     this.base(options);
     this.set({url : '/import/job_status/' + this.id + '.json'});
   },
-  
+
   documentDisplayStatus : function() {
     return this.DOCUMENT_STATUSES[this.get('status')];
   },
-  
+
   update : function() {
     $.get(this.get('url'), {}, _.bind(function(resp) {
       this.set(resp);
@@ -26,35 +26,35 @@ dc.model.ProcessingJob = dc.Model.extend({
       }
     }, this), 'json');
   }
-  
+
 });
 
 
 // ProcessingJob Set
 
 dc.model.ProcessingJobSet = dc.Set.extend({
-  
-  UPDATE_INTERVAL : 3000,
-  
+
+  UPDATE_INTERVAL : 5000,
+
   constructor : function(options) {
     this.base(options);
     _.bindAll('checkForUpdates', this);
   },
-  
+
   startUpdates : function() {
     dc.ui.spinner.hide();
     this._poller = setInterval(this.checkForUpdates, this.UPDATE_INTERVAL);
   },
-  
+
   stopUpdates : function() {
     clearInterval(this._poller);
     this._poller = null;
   },
-  
+
   checkForUpdates : function() {
     _.invoke(this.models(), 'update');
   }
-  
+
 });
 
 window.ProcessingJobs = new dc.model.ProcessingJobSet();
