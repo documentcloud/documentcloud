5ce721a7203ccd7a122b9cf3c41fa0d34c932b49
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-13 14:13:40 -0500

    working MHTML support in Jammit

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -1,16 +1,16 @@
 class WorkspaceController < ApplicationController
-  
+
   RESULTS_REQUEST = /\A\/results/
-  
+
   before_filter :bouncer unless Rails.development?
-  
-  # Main documentcloud.org page. Renders the workspace if logged in or 
+
+  # Main documentcloud.org page. Renders the workspace if logged in or
   # searching, the home page otherwise.
   def index
     return if current_account || results_request?
     render :template => 'search/search', :layout => 'empty'
   end
-  
+
   # Attempt a new signup for DocumentCloud -- includes both the organization and
   # its first account. If everthing's kosher, the journalist is logged in.
   def signup
@@ -22,20 +22,20 @@ class WorkspaceController < ApplicationController
     acc.authenticate(session)
     redirect_to '/'
   end
-  
+
   # /login handles both the login form and the login request.
   def login
     return render unless request.post?
     account = Account.log_in(params[:email], params[:password], session)
     account ? redirect_to('/') : fail(true)
   end
-  
+
   # Logging out clears your entire session.
   def logout
     reset_session
     redirect_to '/'
   end
-  
+
   # Render the TODO.txt file out in public.
   # TODO: (ha!) remove when no longer appropriate.
   def todo
@@ -43,16 +43,16 @@ class WorkspaceController < ApplicationController
     @todo_text.gsub!(/^([A-Z]+:)/, '</ul><h2>\1</h2><ul>').gsub!(/\*(.+?)\n\s*\n/m, '<li>\1</li>')
     render :action => 'todo', :layout => false
   end
-  
-  
+
+
   private
-  
+
   def fail(message)
     @failure = message
   end
-  
+
   def results_request?
     !!request.path.match(RESULTS_REQUEST)
   end
-  
+
 end
