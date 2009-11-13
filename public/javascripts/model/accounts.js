4d9f46d0d4c347d56b69d93f323b003b92598673
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-13 11:10:00 -0500

    new version of jammit -- time to try it on staging

--- public/javascripts/model/accounts.js
+++ public/javascripts/model/accounts.js
@@ -1,41 +1,41 @@
 // Account Model
 
 dc.model.Account = dc.Model.extend({
-  
+
   GRAVATAR_BASE  : 'http://www.gravatar.com/avatar/',
-  
-  DEFAULT_AVATAR : location.protocol + '//' + location.host + '/images/icons/user_blue_32.png',
-  
+
+  DEFAULT_AVATAR : location.protocol + '//' + location.host + '/images/embed/icons/user_blue_32.png',
+
   BLANK_ACCOUNT : {first_name : '', last_name : '', email : ''},
-  
+
   constructor : function(attributes) {
     this.base(attributes || this.BLANK_ACCOUNT);
   },
-  
+
   fullName : function(nonbreaking) {
     var name = this.get('first_name') + ' ' + this.get('last_name');
     return nonbreaking ? name.replace(/\s/g, '&nbsp;') : name;
   },
-  
+
   gravatarURL : function(size) {
     var hash = this.get('hashed_email');
     var fallback = encodeURIComponent(this.DEFAULT_AVATAR);
     return this.GRAVATAR_BASE + hash + '.jpg?s=' + size + '&d=' + fallback;
   }
-  
+
 });
 
 // Account Set
 
 dc.model.AccountSet = dc.model.RESTfulSet.extend({
-  
+
   resource : 'accounts',
-  
+
   // Fetch the account of the logged-in journalist.
   current : function() {
     return this.get(dc.app.accountId);
   }
-  
+
 });
 
 window.Accounts = new dc.model.AccountSet();
