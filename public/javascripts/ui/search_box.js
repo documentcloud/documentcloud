58035d2d22a2487e66e105ed8bbddd1f52a428ca
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-17 09:57:46 -0500

    added search history through the Webkit autosave attribute

--- public/javascripts/ui/search_box.js
+++ public/javascripts/ui/search_box.js
@@ -1,15 +1,16 @@
 // The search controller is responsible for managing document/metadata search.
 dc.ui.SearchBox = dc.View.extend({
-    
+
   PAGE_MATCHER : (/\/p(\d+)$/),
-  
+
   fragment : null,
   currentPage : null,
-    
+
   callbacks : [
-    ['el',  'keydown',  'maybeSearch']
+    ['el',  'keydown',  'maybeSearch'],
+    ['el',  'search',   'searchEvent']
   ],
-  
+
   // Creating a new SearchBox registers #search page fragments.
   constructor : function() {
     this.base({el : $('#search')[0]});
@@ -18,12 +19,12 @@ dc.ui.SearchBox = dc.View.extend({
     _.bindAll('loadSearchResults', 'loadMetadataResults', 'searchByHash', 'saveCurrentSearch', this);
     dc.history.register(/^#search\//, this.searchByHash);
   },
-  
+
   // Shortcut to the searchbox's value.
   value : function(query) {
     return $(this.el).val(query);
   },
-  
+
   // Start a search for a query string, updating the page URL.
   search : function(query, pageNumber) {
     if (dc.app.navigation) dc.app.navigation.tab('search');
@@ -43,7 +44,7 @@ dc.ui.SearchBox = dc.View.extend({
     if (page) params.page = page;
     $.get('/search.json', params, this.loadSearchResults, 'json');
   },
-  
+
   // When searching by the URL's hash value, we need to unescape first.
   searchByHash : function(hash) {
     var page = null;
@@ -51,10 +52,10 @@ dc.ui.SearchBox = dc.View.extend({
     if (pageMatch) {
       var page = pageMatch[1];
       hash = hash.replace(this.PAGE_MATCHER, '');
-    } 
+    }
     this.search(decodeURIComponent(hash), page);
   },
-  
+
   // Callback fired on key press in the search box. We search when they hit
   // return.
   maybeSearch : function(e) {
@@ -62,20 +63,25 @@ dc.ui.SearchBox = dc.View.extend({
       this.search(this.value());
     }
   },
-  
+
+  // Webkit knows how to fire a real "search" event.
+  searchEvent : function(e) {
+    if (!this.outstandingSearch && this.value()) this.search(this.value());
+  },
+
   // Hide the spinner and remove the search lock when finished searching.
   doneSearching : function() {
     dc.ui.spinner.hide();
     this.outstandingSearch = false;
   },
-  
+
   // After the initial search results come back, send out a request for the
   // associated metadata, as long as something was found. Think about returning
   // the metadata right alongside the document JSON.
-  loadSearchResults : function(resp) { 
+  loadSearchResults : function(resp) {
     dc.app.paginator.setQuery(resp.query);
-    Documents.refresh(_.map(resp.documents, function(m){ 
-      return new dc.model.Document(m); 
+    Documents.refresh(_.map(resp.documents, function(m){
+      return new dc.model.Document(m);
     }));
     $('#query_container').html(new dc.ui.Query(resp.query).render().el);
     $('#document_list_container').html((new dc.ui.DocumentList({set : Documents})).render().el);
@@ -87,8 +93,8 @@ dc.ui.SearchBox = dc.View.extend({
     var docIds = _.pluck(resp.documents, 'id');
     $.get('/documents/metadata.json', {'ids[]' : docIds}, this.loadMetadataResults, 'json');
   },
-  
-  // When the metadata results come back, render the entity list in the sidebar 
+
+  // When the metadata results come back, render the entity list in the sidebar
   // afresh.
   loadMetadataResults : function(resp) {
     Metadata.refresh();
@@ -98,7 +104,7 @@ dc.ui.SearchBox = dc.View.extend({
     $('#metadata_container').html(mView.render().el);
     this.doneSearching();
   },
-  
+
   saveCurrentSearch : function() {
     var options = {anchor : this.saveSearchButton, position: 'left center', left: -12, top: -1};
     SavedSearches.create(new dc.model.SavedSearch({query : this.value()}), null, {success : function() {
@@ -108,16 +114,16 @@ dc.ui.SearchBox = dc.View.extend({
       dc.ui.notifier.show(_.extend(options, {text : 'search already saved'}));
     }});
   },
-  
+
   addSaveSearchButton : function() {
     this.saveSearchButton = $.el('div', {id : 'save_search', 'class' : 'minibutton tab_content search_tab_content'}, 'save this search');
     $(document.body).append(this.saveSearchButton);
     $(this.saveSearchButton).bind('click', this.saveCurrentSearch);
   },
-  
+
   contextSensitiveSaveButton : function(query) {
     if (!this.saveSearchButton) return;
     $(this.saveSearchButton).css({opacity : SavedSearches.find(query) ? 0 : 1});
   }
-  
+
 });
