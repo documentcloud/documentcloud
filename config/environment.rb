021d14780660413b4e494a3f4025c2c3776e4aed
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-19 14:10:57 -0500

    specifying libxml rightaws communication

--- config/environment.rb
+++ config/environment.rb
@@ -44,8 +44,8 @@ Rails::Initializer.run do |config|
   config.gem 'postgres',              :version => '>= 0.7.9.2008.01.28'
   config.gem 'jammit',                :version => '>= 0.2.1'
 
-
   require 'json/add/rails'
+  RightAws::RightAWSParser.xml_lib = 'libxml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
