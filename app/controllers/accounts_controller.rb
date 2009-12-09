20545cdbdf9afd6efaaf35f718d807eea8ea22e9
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-09 17:14:13 -0500

    comments comments

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,13 @@
+# The AccountsController is responsible for account management -- adding users
+# activating accounts, updating email addresses and the like.
 class AccountsController < ApplicationController
   layout 'workspace'
 
   before_filter :login_required, :except => [:enable]
   before_filter :bouncer, :only => [:enable] unless Rails.env.development?
 
+  # Enabling an account continues the second half of the signup process,
+  # allowing the journalist to set their password, and logging them in.
   def enable
     return render if request.get?
     key = SecurityKey.find_by_key(params[:key])
@@ -16,10 +20,14 @@ class AccountsController < ApplicationController
     redirect_to '/'
   end
 
+  # Requesting /accounts returns the list of accounts in your logged-in organization.
   def index
     json 'accounts' => current_organization.accounts
   end
 
+  # Creating a new account creates a pending account, with a security key
+  # instead of a password.
+  # TODO: We can't sent email from EC2 without it getting flagged as spam.
   def create
     attributes = pick_params(:first_name, :last_name, :email)
     account = current_organization.accounts.create(attributes)
@@ -35,6 +43,8 @@ class AccountsController < ApplicationController
     json account
   end
 
+  # Removing an account will preserve their uploaded documents, for the
+  # moment -- perhaps private docs should be destroyed.
   def destroy
     current_organization.accounts.find(params[:id]).destroy
     json nil
