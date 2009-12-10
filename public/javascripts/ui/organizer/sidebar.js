b0329ee8bfeb2465a9f1f8bc278646b4c7aeea94
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-10 14:41:36 -0500

    split apart the filter and add label into two fields

--- public/javascripts/ui/organizer/sidebar.js
+++ public/javascripts/ui/organizer/sidebar.js
@@ -1,29 +1,34 @@
 dc.ui.OrganizerSidebar = dc.View.extend({
-  
+
   id : 'organizer_sidebar',
-  
+
   callbacks : [
     ['#new_label_button', 'click',    'saveNewLabel'],
-    ['#label_input',      'keyup',    'onLabelInput']
+    ['#label_input',      'keyup',    'onLabelInput'],
+    ['#filter_box',       'keyup',    'autofilter']
   ],
-  
+
   constructor : function(options) {
     this.base(options);
     this.organizer = this.options.organizer;
   },
-  
+
   render : function() {
     $(this.el).append(JST.organizer_sidebar({}));
     this.labelInputEl = $('#label_input', this.el);
+    this.filterEl = $('#filter_box', this.el);
     this.setCallbacks();
     return this;
   },
-  
+
+  autofilter : function(e) {
+    this.organizer.autofilter(this.filterEl.val());
+  },
+
   onLabelInput : function(e) {
-    this.organizer.autofilter(this.labelInputEl.val());
     if (e.keyCode && e.keyCode === 13) return this.saveNewLabel(e);
   },
-  
+
   saveNewLabel : function(e) {
     var me = this;
     var input = this.labelInputEl;
@@ -36,10 +41,10 @@ dc.ui.OrganizerSidebar = dc.View.extend({
     var label = new dc.model.Label({title : title});
     Labels.create(label, null, {error : function() { Labels.remove(label); }});
   },
-  
+
   warnAlreadyExists : function() {
     dc.ui.notifier.show({text : 'label already exists', anchor : this.labelInputEl, position : '-left bottom', top : 6, left : 1});
   }
-  
+
 });
 
