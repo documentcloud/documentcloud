51b5abcaf659f16c59330800f446db4b0c6cf855
Jeremy Ashkenas <jashkenas@gmail.com>
2010-01-14 13:51:54 -0500

    waiting for CloudCrowd to come line

--- db/migrate/20100114170350_regenerate_thumbnails.rb
+++ db/migrate/20100114170350_regenerate_thumbnails.rb
@@ -2,6 +2,8 @@
 
 class RegenerateThumbnails < ActiveRecord::Migration
   def self.up
+    # wait for a second for CloudCrowd to come online.
+    sleep 2
     ids = Document.all(:select => 'id').map {|d| d.id }
     RestClient.post(DC_CONFIG['cloud_crowd_server'] + '/jobs', {:job => {
       'action'  => 'regenerate_thumbnails',
