80607e20888282d4276ea3bc75851b3d4ff0727e
Jeremy Ashkenas <jashkenas@gmail.com>
2010-01-15 14:51:42 -0500

    initial commit -- moved all old ui assets into v1 folder ... work on the new one progresses in v2 / the DesignController

--- public/javascripts/app/downloader.js
+++ public/javascripts/app/downloader.js
@@ -1,4 +1,8 @@
 // Simple downloader for the time being. Lazy-load the download iframe.
 dc.app.download = function(url) {
-  $('#hidden_iframe').attr({src : url});
+  if (!this.iframe) {
+    this.iframe = $.el("iframe", {id : 'hidden_iframe', name : 'hidden_iframe', src : 'about:blank', 'class' : 'hidden_iframe'});
+    $(document.body).append(this.iframe);
+  }
+  $(this.iframe).attr({src : url});
 };
