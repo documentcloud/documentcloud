ab467fc4896406da402e5166ff20a9e85db90806
Jeremy Ashkenas <jashkenas@gmail.com>
2010-02-09 14:54:42 -0500

    got the timeline working semi-alright in IE

--- public/javascripts/ui/common/tooltip.js
+++ public/javascripts/ui/common/tooltip.js
@@ -1,7 +1,6 @@
 dc.ui.Tooltip = dc.View.extend({
 
   id        : 'tooltip',
-  className : 'gradient_white',
 
   OFFSET : 5,
 
@@ -9,26 +8,37 @@ dc.ui.Tooltip = dc.View.extend({
     this.base();
     this._open = false;
     _.bindAll(this, 'hide', 'show');
+    $(this.el).append($.el('div', {id: 'tooltip_background', 'class' : 'gradient_white'}));
+    $(this.el).append($.el('div', {id: 'tooltip_text'}));
+    this.content = $('#tooltip_text', this.el);
     $(document.body).append(this.el);
   },
 
   show : function(options) {
-    this._open    = true;
     options       = _.extend(this.defaultOptions(), options);
     options.left += this.OFFSET;
     options.top  += this.OFFSET;
     this.setMode(options.mode, 'style');
-    $(this.el).html(options.text)
-              .fadeIn('fast')
-              .css({top : options.top, left : options.left});
+    this.content.html(options.text);
+    $(this.el).css({top : options.top, left : options.left});
+    if (!this._open) this.fadeIn();
     $(document).bind('mouseover', this.hide);
+    this._open    = true;
   },
 
   hide : function() {
     if (!this._open) return;
     this._open = false;
     $(document).unbind('mouseover', this.hide);
-    $(this.el).fadeOut('fast');
+    this.fadeOut();
+  },
+
+  fadeIn : function() {
+    $.browser.msie ? $(this.el).show() : $(this.el).fadeIn('fast');
+  },
+
+  fadeOut : function() {
+    $.browser.msie ? $(this.el).hide() : $(this.el).fadeOut('fast');
   },
 
   defaultOptions : function() {
