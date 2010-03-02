3afdba378ae01f8bb006b210ae22a487989c073f
Jeremy Ashkenas <jashkenas@gmail.com>
2010-03-02 12:22:00 -0500

    trying again with the fully-qualified username

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address        => 'mail.documentcloud.org',
   :port           => 26,
-  :user_name      => 'smtp',
+  :user_name      => 'smtp@documentcloud.org',
   :password       => SECRETS['smtp_password'],
   :authentication => :plain
 }
