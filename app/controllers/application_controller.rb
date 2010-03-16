6439dde24ff999c88af8909f9acac156aa361761
Jeremy Ashkenas <jashkenas@gmail.com>
2010-03-16 14:27:35 -0400

    allowing admins (us) to view any document for debugging purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,12 +54,16 @@ class ApplicationController < ActionController::Base
     session['account_id'] && session['organization_id']
   end
 
+  def admin?
+    logged_in? && current_organization.id == 1
+  end
+
   def login_required
     logged_in? || forbidden
   end
 
   def admin_required
-    (logged_in? && current_organization.id == 1) || forbidden
+    admin? || forbidden
   end
 
   def current_account
