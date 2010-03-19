876c647822123a741cee68c33fe54d21042c9ec7
Jeremy Ashkenas <jashkenas@gmail.com>
2010-03-19 15:24:03 -0400

    and back to past two weeks

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,8 +9,8 @@ class AdminController < ApplicationController
     @average_entity_count   = DC::Statistics.average_entity_count.to_json
     @average_page_count     = DC::Statistics.average_page_count.to_json
     @total_pages            = DC::Statistics.total_pages.to_json
-    @daily_documents        = keys_to_timestamps(DC::Statistics.daily_documents(1.month.ago)).to_json
-    @daily_pages            = keys_to_timestamps(DC::Statistics.daily_pages(1.month.ago)).to_json
+    @daily_documents        = keys_to_timestamps(DC::Statistics.daily_documents(2.weeks.ago)).to_json
+    @daily_pages            = keys_to_timestamps(DC::Statistics.daily_pages(2.weeks.ago)).to_json
     @documents_per_account  = DC::Statistics.documents_per_account.to_json
     @pages_per_account      = DC::Statistics.pages_per_account.to_json
     @accounts               = Account.all.to_json
