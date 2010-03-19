974ce5871d987c83b0f0183004170f03cc3714f6
Jeremy Ashkenas <jashkenas@gmail.com>
2010-03-19 10:18:58 -0400

    making the admin signup page a little bit nicer

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  layout nil
+  layout 'workspace'
 
   before_filter :admin_required
 
@@ -7,15 +7,13 @@ class AdminController < ApplicationController
   # its first account. If everthing's kosher, the journalist is logged in.
   # NB: This needs to stay access controlled by the bouncer throughout the beta.
   def signup
-    return render(:layout => 'workspace') unless request.post?
+    return render unless request.post?
     org = Organization.create(params[:organization])
     return fail(org.errors.full_messages.first) if org.errors.any?
     acc = Account.create(params[:account].merge({:organization => org, :role => Account::ADMINISTRATOR}))
     return org.destroy && fail(acc.errors.full_messages.first) if acc.errors.any?
     acc.send_login_instructions
-    render :text => "Account Created. Welcome email sent to #{acc.email}"
-    # acc.authenticate(session)
-    # redirect_to '/'
+    @success = "Account Created. Welcome email sent to #{acc.email}."
   end
 
   # Login as a given account, without needing a password.
@@ -30,7 +28,7 @@ class AdminController < ApplicationController
   def todo
     @todo_text = File.read("#{Rails.root}/TODO")
     @todo_text.gsub!(/^(\w+[^\n]+:)/, '</ul><h2>\1</h2><ul>').gsub!(/^\s+\*(.+?)\n\s*\n/m, '<li>\1</li>')
-    render :action => 'todo', :layout => false
+    render :action => 'todo', :layout => nil
   end
 
   def test_exception_notifier
@@ -38,7 +36,7 @@ class AdminController < ApplicationController
   end
 
   def test_embedded_viewer
-
+    render :layout => nil
   end
 
 
