ba9ce7454059f1f5f3e2c6eaacf55aeb160ac331
Jeremy Ashkenas <jashkenas@gmail.com>
2010-04-01 16:45:37 -0400

    simplifying bindable

--- public/javascripts/lib/bindable.js
+++ public/javascripts/lib/bindable.js
@@ -11,55 +11,43 @@ dc.util.Bindable = {
 
     // e -> event object
     // callback -> callback function
-    // scope -> optional scoping object
-    // message -> optional message to be logged every time the callback fires
-    bind : function(e, callback, scope, message) {
+    bind : function(e, callback) {
       if (!e)         throw new Error('dc.util.Bindable: Undefined event');
       if (!callback)  throw new Error('dc.util.Bindable: Undefined callback');
-      var calls = (this.__callbacks__ = this.__callbacks__ || {});
+      var calls = (this._callbacks = this._callbacks || {});
       var list = (calls[e] = calls[e] || []);
-      list.push({
-        callback : callback,
-        scope : scope,
-        message : message
-      });
+      list.push(callback);
     },
 
     // Remove one or more callbacks.
-    unbind : function(e, callback, scope) {
-      var calls = this.__callbacks__;
+    unbind : function(e, callback) {
+      var calls = this._callbacks;
       if (!calls) return;
       for (var ev in calls) {
         if (e && e != ev) continue; // Skip events we aren't trying to undo.
         var list = calls[ev], i = (list && list.length) || 0;
-        while (i--) {
-          var bound = list[i];
-          if (bound && (!callback || callback == bound.callback) &&
-            (!scope || scope == bound.scope)) {
-            list[i] = null;
-          }
-        }
+        while (i--) if (callback == list[i]) list[i] = null;
       }
     },
 
     // Remove all bound callbacks.
     unbindAll : function() {
-      delete this.__callbacks__;
+      delete this._callbacks;
     },
 
     // Fire an event, triggering all bound callbacks.
     fire : function(e) {
       if (!e) throw new Error('dc.util.Bindable: Undefined event');
-      var calls = this.__callbacks__;
+      var calls = this._callbacks;
 
       for (var j=0; j<2; j++) {
         var ev = j ? e : dc.util.Bindable.ALL;
         var list = calls && calls[ev], i = (list && list.length) || 0;
         var thin = false;
         while(i--) {
-          var bound = list[i];
-          if (bound) {
-            bound.callback.apply(this, arguments);
+          var callback = list[i];
+          if (callback) {
+            callback.apply(this, arguments);
           } else {
             thin = true;
           }
