d91b5690fdbae5c21caf712b6cf2367041b0d922
Jeremy Ashkenas <jashkenas@gmail.com>
2010-04-07 12:23:01 -0400

    s/slug/group

--- public/javascripts/ui/accounts/account_dialog.js
+++ public/javascripts/ui/accounts/account_dialog.js
@@ -10,7 +10,7 @@ dc.ui.AccountDialog = dc.ui.Dialog.extend({
   constructor : function() {
     this.base({
       mode      : 'custom',
-      title     : dc.app.organization.name + ' <span class="slug">slug: ' + dc.app.organization.slug + '</span>'
+      title     : dc.app.organization.name + ' <span class="group">group: ' + dc.app.organization.slug + '</span>'
     });
     _.bindAll(this, '_renderAccounts');
     this._rendered = false;
