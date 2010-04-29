cdd0fbaf21b518f5c5427e97f936b8de6400a3f9
Jeremy Ashkenas <jashkenas@gmail.com>
2010-04-29 12:11:25 -0400

    ...

--- public/javascripts/ui/common/panel.js
+++ public/javascripts/ui/common/panel.js
@@ -6,11 +6,17 @@ dc.ui.Panel = dc.View.extend({
   render : function() {
     $(this.el).html(JST.workspace_panel({}));
     this.content = $('.panel_content', this.el);
+    this._setMinHeight();
+    $(window).resize(_.bind(this._setMinHeight, this));
     return this;
   },
 
   add : function(containerName, view) {
     $('#' + containerName + '_container', this.el).append(view);
+  },
+
+  _setMinHeight : function() {
+    $(this.el).css({"min-height": $(window).height() - 120});
   }
 
 });
