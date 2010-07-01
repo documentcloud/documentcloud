b5f4eb30de615a2b82c06b9517c658f7a53138f0
Jeremy Ashkenas <jashkenas@gmail.com>
2010-07-01 15:22:17 -0400

    merged in the new notes branch.

--- public/javascripts/ui/documents/note.js
+++ public/javascripts/ui/documents/note.js
@@ -20,7 +20,7 @@ dc.ui.Note = dc.View.extend({
   },
 
   render : function() {
-    var data = _.extend(this.model.attributes(), {note : this.model, coords : this.model.coordinates()});
+    var data = _.extend(this.model.attributes(), {note : this.model});
     $(this.el).html(JST.document_note(data));
     this.setMode('display', 'visible');
     this.setCallbacks();
