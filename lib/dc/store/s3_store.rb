587df37bdf34ed2d317b873572822e44b2849eba
Jeremy Ashkenas <jashkenas@gmail.com>
2010-07-16 17:08:53 -0400

    trying out something with authorized urls

--- lib/dc/store/s3_store.rb
+++ lib/dc/store/s3_store.rb
@@ -33,7 +33,7 @@ module DC
       end
 
       def authorized_url(path)
-        s3.interface.get_link(bucket, path, AUTH_PERIOD)
+        s3.interface.generate_link('GET', {:url => File.join(web_root, path)}, AUTH_PERIOD)
       end
 
       def list(path)
