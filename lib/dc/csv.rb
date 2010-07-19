c69c38ffc08002b6b4a3f8005c099bc2492d7f4c
Samuel Clay <samuel@documentcloud.org>
2010-07-19 16:35:42 -0400

    Download CSV for accounts and top documents.

---
+++ lib/dc/csv.rb
@@ -0,0 +1,24 @@
+require 'FasterCSV'
+
+module DC
+  
+  module CSV
+    
+    def self.generate_csv(records, keys=nil)
+      keys ||= records.first.keys
+    
+      csv_string = FasterCSV.generate do |csv|
+        csv << keys
+        records.each do |record|
+          record_array = []
+          keys.each {|key| record_array.push record[key] }
+          csv << record_array
+        end
+      end
+
+      return csv_string
+    end
+    
+  end
+  
+end
