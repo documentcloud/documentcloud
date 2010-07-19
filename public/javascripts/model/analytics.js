54d04865f2616003685dd189817c7e90e5721a02
Jeremy Ashkenas <jashkenas@gmail.com>
2010-07-19 11:27:37 -0400

    remove unneeded closure from analytics.js

--- public/javascripts/model/analytics.js
+++ public/javascripts/model/analytics.js
@@ -5,12 +5,9 @@ dc.analytics = {
     window._gaq = window._gaq || [];
     _gaq.push(['_setAccount', 'UA-9312438-1']);
     _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   },
 
   // Registers Google Analytics with the history hash change callbacks
