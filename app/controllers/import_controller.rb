8993b368d71f9e04b334bcc9e306f1fcb1a8e480
Samuel Clay <samuel@documentcloud.org>
2010-07-21 18:03:37 -0400

    git add app/controllers/import_controller.rb! Reindexing document when cloud-crowd successfully calls /import/cloud_crowd. Shouldn't documents be indexed at this point? Handling manually, but I suspect there must be a better way.

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -17,11 +17,12 @@ class ImportController < ApplicationController
   # If the document failed to import, we remove it.
   def cloud_crowd
     job = JSON.parse(params[:job])
+    record = ProcessingJob.find_by_cloud_crowd_id(job['id'])
     if job['status'] != 'succeeded'
       logger.error("Document import failed: " + job.inspect)
-      record = ProcessingJob.find_by_cloud_crowd_id(job['id'])
       record.document.update_attributes(:access => DC::Access::ERROR) if record && record.document
     end
+    record.document.index
     ProcessingJob.destroy_all(:cloud_crowd_id => job['id'])
     render :text => '201 Created', :status => 201
   end
