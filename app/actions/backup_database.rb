eaa1cafde9573c5a8ede2cce3d6e1cad95b1856f
Jeremy Ashkenas <jashkenas@gmail.com>
2010-08-06 09:50:04 -0400

    DRY-ing db backup task

--- app/actions/backup_database.rb
+++ app/actions/backup_database.rb
@@ -3,22 +3,25 @@ require File.dirname(__FILE__) + '/support/setup'
 class BackupDatabase < CloudCrowd::Action
 
   def process
-    date      = Date.today.to_s
     config    = Rails::Configuration.new.database_configuration[Rails.env]
-    db        = MAIN_DB[Rails.env]['database']
-    host      = config['host'] ? "-h #{config['host']}" : ''
-    username  = config['username']
-    pass      = config['password']
+    @host     = config['host'] ? "-h #{config['host']}" : ''
+    @username = config['username']
+    @pass     = config['password']
     Dir.mktmpdir do |temp_dir|
-      dump    = File.join(temp_dir, "#{db}.dump")
-      system "PGPASSWORD=\"#{pass}\" pg_dump -U #{username} #{host} -Fc #{db} > #{dump}"
-      DC::Store::AssetStore.new.save_database_backup('dcloud', dump)
-      db      = ANALYTICS_DB[Rails.env]['database']
-      dump    = File.join(temp_dir, "#{db}.dump")
-      system "PGPASSWORD=\"#{pass}\" pg_dump -U #{username} #{host} -Fc #{db} > #{dump}"
-      DC::Store::AssetStore.new.save_database_backup('analytics', dump)
+      @temp_dir = temp_dir
+      backup MAIN_DB['database']
+      backup ANALYTICS_DB['database']
     end
     true
+  end
+
+
+  private
+
+  def backup(db_name)
+    dump = File.join(@temp_dir, "#{db_name}.dump")
+    system "PGPASSWORD=\"#{@pass}\" pg_dump -U #{@username} #{@host} -Fc #{db_name} > #{dump}"
+    DC::Store::AssetStore.new.save_database_backup(db_name, dump)
   end
 
 end
