fd6ceefefeebfbbd79829976328d8b0bc897067f
Jeremy Ashkenas <jashkenas@gmail.com>
2010-08-06 09:43:29 -0400

    fixing db config in backup job

--- app/actions/backup_database.rb
+++ app/actions/backup_database.rb
@@ -5,7 +5,7 @@ class BackupDatabase < CloudCrowd::Action
   def process
     date      = Date.today.to_s
     config    = Rails::Configuration.new.database_configuration[Rails.env]
-    db        = config['database']
+    db        = MAIN_DB[Rails.env]['database']
     host      = config['host'] ? "-h #{config['host']}" : ''
     username  = config['username']
     pass      = config['password']
@@ -13,7 +13,7 @@ class BackupDatabase < CloudCrowd::Action
       dump    = File.join(temp_dir, "#{db}.dump")
       system "PGPASSWORD=\"#{pass}\" pg_dump -U #{username} #{host} -Fc #{db} > #{dump}"
       DC::Store::AssetStore.new.save_database_backup('dcloud', dump)
-      db      = ANALYTICS_DB[Rails.env]
+      db      = ANALYTICS_DB[Rails.env]['database']
       dump    = File.join(temp_dir, "#{db}.dump")
       system "PGPASSWORD=\"#{pass}\" pg_dump -U #{username} #{host} -Fc #{db} > #{dump}"
       DC::Store::AssetStore.new.save_database_backup('analytics', dump)
