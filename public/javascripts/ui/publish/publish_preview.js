294d2007946fbf03ae14ac97d4d211d66508d438
Samuel Clay <samuel@ofbrooklyn.com>
2010-08-31 14:18:41 -0400

    Retooling how options work in embed dialog.

--- public/javascripts/ui/publish/publish_preview.js
+++ public/javascripts/ui/publish/publish_preview.js
@@ -1,26 +1,51 @@
 dc.ui.PublishPreview = dc.ui.Dialog.extend({
   
   callbacks : {
-    '.ok.click'     : 'confirm',
-    'input.change'  : 'update',
-    'select.change' : 'update',
-    'input.keyup'   : 'update',
-    'input.focus'   : 'update',
-    'input.click'   : 'update',
-    'input[name=zoom_specific].focus' : '_selectZoomSpecific',
-    'publish_preview_new_window_link.click' : 'previewEmbedNewWindow'
+    'input[name=zoom_specific].focus'        : '_selectZoomSpecific',
+    'input[name=viewer_size].change'         : '_selectViewerSize',
+    'input[name=width].focus'                : '_selectViewerFixed',
+    'input[name=height].focus'               : '_selectViewerFixed',
+    '.publish_preview_new_window_link.click' : 'previewEmbedNewWindow',
+    '.publish_advanced.click'                : '_showAdvancedOptions',
+    '.ok.click'                              : 'confirm',
+    'input.change'                           : 'update',
+    'select.change'                          : 'update',
+    'input.keyup'                            : 'update',
+    'input.focus'                            : 'update',
+    'input.click'                            : 'update'
   },
   
-  DEFAULT_VIEWER_OPTIONS : {
-    container: '#document-viewer',
-    viewer_size: 'full',
-    width: 600,
-    height: 800,
-    zoom: 'auto',
-    showSidebar: false,
-    showText: true,
-    showHeader: true,
-    enableUrlChanges: false
+  VIEWER_DEFAULTS : {
+    zoom             : 700,
+    showSidebar      : true,
+    showText         : true,
+    showSearch       : true,
+    showHeader       : true,
+    enableUrlChanges : true
+  },
+  
+  DEFAULT_FULLSCREEN_OPTIONS : {
+    container        : '#document-viewer',
+    viewer_size      : 'full',
+    width            : 600,
+    height           : 800,
+    zoom             : 700,
+    showSidebar      : true,
+    showText         : true,
+    showHeader       : true,
+    enableUrlChanges : true
+  },
+  
+  DEFAULT_FIXED_OPTIONS : {
+    container        : '#document-viewer',
+    viewer_size      : 'fixed',
+    width            : 600,
+    height           : 800,
+    zoom             : 'auto',
+    showSidebar      : false,
+    showText         : true,
+    showHeader       : true,
+    enableUrlChanges : false
   },
   
   constructor : function(doc) {
@@ -48,6 +73,7 @@ dc.ui.PublishPreview = dc.ui.Dialog.extend({
     }
     this.update();
     this.center();
+    _.bindAll(this, '_showAdvancedOptions');
     return this;
   },
   
@@ -84,18 +110,26 @@ dc.ui.PublishPreview = dc.ui.Dialog.extend({
   
   _loadPreferences : function() {
     var userOpts = JSON.parse(dc.app.preferences.get('embed_options')) || {};
+    
+    this.setOptions(userOpts);
+  },
+  
+  setOptions : function(opts) {
     var $form = $('form', this.el);
     var $formElements = $('input, select', $form);
     
     $formElements.each(function(i) {
       var $this = $(this);
-      if ($this.attr('name') in userOpts) {
+      var inputName = $this.attr('name');
+      if (inputName in opts) {
         if ($this.is('input[type=radio]')) {
-          if ($this.val() == userOpts[$this.attr('name')]) {
+          if (inputName == 'zoom' && opts[inputName] != 'auto' && $this.val() == 'specific') {
+           $this.attr('checked', true);
+          } else if ($this.val() == opts[inputName]) {
             $this.attr('checked', true);
           }
         } else {
-          $this.val(userOpts[$this.attr('name')]);
+          $this.val(opts[$this.attr('name')]);
         }
       } else {
         if ($this.is('input[type=checkbox]')) {
@@ -108,28 +142,29 @@ dc.ui.PublishPreview = dc.ui.Dialog.extend({
   },
   
   _renderEmbedCode : function() {
-    var userOpts = $('form.publish_options', this.el).serializeJSON();
-    _.each(this.DEFAULT_VIEWER_OPTIONS, _.bind(function(v, k) {
-      if (!(k in userOpts)) userOpts[k] = false;
-      else if (userOpts[k] == 'on') userOpts[k] = true;
+    var $form = $('form.publish_options', this.el);
+    var userOpts = {};
+    
+    userOpts['viewer_size'] = $('input[name=viewer_size]:checked', $form).val();
+    if (userOpts['viewer_size'] == 'fixed') {
+      userOpts['width'] = parseInt($('input[name=width]', $form).val(), 10);
+      userOpts['height'] = parseInt($('input[name=height]', $form).val(), 10);
+    }
+    if ($('input[name=zoom]:checked', $form).val() == 'auto') {
+      userOpts['zoom'] = 'auto';
+    } else {
+      userOpts['zoom'] = parseInt($('input[name=zoom_specific]', $form).val(), 10);
+    }
+    userOpts['showSidebar'] = $('input[name=showSidebar]').attr('checked');
+    userOpts['showText'] = $('input[name=showText]').attr('checked');
+    userOpts['showHeader'] = $('input[name=showHeader]').attr('checked');
+    userOpts['enableUrlChanges'] = $('input[name=enableUrlChanges]').attr('checked');
 
-      // Zoom override
-      if (k == 'zoom' && userOpts[k] == 'specific') {
-        var zoom = parseInt(userOpts['zoom_specific'], 10);
-        if (zoom >= 100) {
-          userOpts['zoom'] = zoom;
-        } else {
-          userOpts['zoom'] = 'auto';
-        }
-      }
-      
-      // Viewer size override
-      if (k == 'viewer_size' && userOpts[k] == 'fixed') {
-        userOpts['width'] = parseInt(userOpts['width'], 10);
-        userOpts['height'] = parseInt(userOpts['height'], 10);
-      }
-    }, this));
-    var options = $.extend({}, this.DEFAULT_VIEWER_OPTIONS, userOpts);
+    var defaultOptions = userOpts['viewer_size'] == 'fixed' ?
+                         this.DEFAULT_FIXED_OPTIONS :
+                         this.DEFAULT_FULLSCREEN_OPTIONS;
+    
+    var options = $.extend({}, defaultOptions, userOpts);
 
     if (options['viewer_size'] == 'full') {
       delete options['width'];
@@ -138,9 +173,19 @@ dc.ui.PublishPreview = dc.ui.Dialog.extend({
     delete options['viewer_size'];
     delete options['zoom_specific'];
     
+    // Remove options that are the same as defaults
+    _.each(options, _.bind(function(v, k) {
+      if (typeof v == 'boolean' && v == this.VIEWER_DEFAULTS[k]) {
+        delete options[k];
+      }
+    }, this));
+    
+    options = _.map(options, function(value, key) {
+      return key + ": " + (typeof value == 'string' ? "\""+value+"\"" : value);
+    });
     $('.publish_embed_code', this.el).html(JST['document/embed_dialog']({
       doc: this.embedDoc,
-      options: options
+      options: options.join(',&#10;    ')
     }));
     
     this.embedOptions = options;
@@ -168,6 +213,40 @@ dc.ui.PublishPreview = dc.ui.Dialog.extend({
   
   _selectZoomSpecific : function() {
     $('input#publish_option_zoom_specific', this.el).attr('checked', true);
+  },
+  
+  _selectViewerFixed : function() {
+    var $viewerFixed = $('input#publish_option_viewer_size_fixed', this.el);
+    if (!$viewerFixed.attr('checked')) {
+      $viewerFixed.attr('checked', true);
+      this._selectViewerSize();
+    }
+  },
+  
+  _selectViewerSize : function() {
+    var viewer = $('input[name=viewer_size]:checked').val();
+
+    if (viewer == 'fixed') {
+      this.setOptions(this.DEFAULT_FIXED_OPTIONS);
+      $('input[name=zoom_specific]', this.el).val(this.DEFAULT_FULLSCREEN_OPTIONS['zoom']);
+    } else if (viewer == 'full') {
+      this.setOptions(this.DEFAULT_FULLSCREEN_OPTIONS);
+      $('input#publish_option_zoom_specific', this.el).attr('checked', true);
+      $('input[name=zoom_specific]', this.el).val(this.DEFAULT_FULLSCREEN_OPTIONS['zoom']);
+    }
+  },
+  
+  _showAdvancedOptions : function() {
+    var $advancedLink = $('.publish_advanced', this.el);
+    var $advancedOptions = $('.publish_options_group_advanced', this.el);
+    
+    if ($advancedLink.hasClass('active')) {
+      $advancedLink.removeClass('active');
+      $advancedOptions.slideUp(250);
+    } else {
+      $advancedLink.addClass('active');
+      $advancedOptions.slideDown(250);
+    }
   }
   
 });
