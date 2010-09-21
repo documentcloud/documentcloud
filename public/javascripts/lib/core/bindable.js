5cbf97b56cdbc33b1502a9554fcb06c6741042b9
Jeremy Ashkenas <jashkenas@gmail.com>
2010-09-21 11:06:45 -0400

    revisiting bindable... expanding the unbind() api, optimizing fire()

--- public/javascripts/lib/core/bindable.js
+++ public/javascripts/lib/core/bindable.js
@@ -2,43 +2,44 @@ dc.model.Bindable = {
 
   // Bind an event on this object to a given callback.
   bind : function(e, callback) {
-    var calls = (this._callbacks = this._callbacks || {});
-    var list  = (this._callbacks[e] = this._callbacks[e] || []);
+    var calls = this._callbacks || (this._callbacks = {});
+    var list  = this._callbacks[e] || (this._callbacks[e] = []);
     list.push(callback);
+    return this;
   },
 
-  // Remove one callback.
+  // Remove one or many callbacks.
   unbind : function(e, callback) {
-    var calls = this._callbacks;
-    if (!calls) return;
-    for (var ev in calls) {
-      if (e != ev) continue;
-      var list = calls[ev];
-      for (var i = 0, l = list.length; i < l; i++) {
-        if (callback == list[i]) {
-          list.splice(i, 1);
-          break;
+    var calls;
+    if (!e) {
+      this._callbacks = {};
+    } else if (calls = this.callbacks) {
+      if (!callback) {
+        calls[e] = [];
+      } else {
+        var list = calls[e];
+        for (var i = 0, l = list.length; i < l; i++) {
+          if (callback === list[i]) {
+            list.splice(i, 1);
+            break;
+          }
         }
       }
-      break;
     }
-  },
-
-  // Remove all bound callbacks.
-  unbindAll : function() {
-    delete this._callbacks;
+    return this;
   },
 
   // Fire an event, triggering all bound callbacks.
   fire : function(e) {
     var calls = this._callbacks;
     for (var i = 0; i < 2; i++) {
-      var list = calls && calls[i ? e : 'all'];
+      var list = calls && calls[i ? 'all' : e];
       if (!list) continue;
-      for (var j = 0; j < list.length; j++) {
+      for (var j = 0, l = list.length; j < l; j++) {
         list[j].apply(this, arguments);
       }
     }
+    return this;
   }
 
 };
