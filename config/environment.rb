14797f4b13bc9403586d97e4b82bf0765721f41b
Jeremy Ashkenas <jashkenas@gmail.com>
2010-09-28 10:25:17 -0400

    Upgrading to Rails 2.3.9

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -36,7 +36,8 @@ Rails::Initializer.run do |config|
   config.gem 'fastercsv',             :version => '>= 1.5.3'
 
   # Middleware
-  config.load_paths << "#{Rails.root}/app/middleware"
+  paths = config.respond_to?(:autoload_paths) ? config.autoload_paths : config.load_paths
+  paths << "#{Rails.root}/app/middleware"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
