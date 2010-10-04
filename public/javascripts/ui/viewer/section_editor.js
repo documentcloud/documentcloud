bf7dd43a1650a7c9ff1937ff53ab716c79382c63
Jeremy Ashkenas <jashkenas@gmail.com>
2010-10-04 10:42:14 -0400

    Cleaning up section editing ... no more 'end page'...

--- public/javascripts/ui/viewer/section_editor.js
+++ public/javascripts/ui/viewer/section_editor.js
@@ -10,14 +10,14 @@ dc.ui.SectionEditor = Backbone.View.extend({
     this.sections = _.sortBy(currentDocument.api.getSections(), function(s){ return parseInt(s.pageNumber, 10); });
     this.dialog = new dc.ui.Dialog({
       title       : 'Edit Sections',
-      information : 'Please choose a title and page range for each section.',
+      information : 'Please add a title and page number for each section.',
       id          : 'section_editor',
       mode        : 'confirm',
       saveText    : 'Save',
       onClose     : _.bind(function(){ this.dialog = null; }, this),
       onConfirm   : _.bind(function(){ return this.saveSections(this.serializeSections()); }, this)
     }).render();
-    this.sectionsEl = $(this.make('ol', {id : 'section_rows'}));
+    this.sectionsEl = $(this.make('ul', {id : 'section_rows'}));
     this.removeEl   = $(this.make('div', {'class' : 'minibutton warn remove_all'}, 'Remove All'));
     this.removeEl.bind('click', this.removeAllSections);
     this.dialog.append(this.sectionsEl);
@@ -49,12 +49,16 @@ dc.ui.SectionEditor = Backbone.View.extend({
   },
 
   serializeSections : function() {
-    var sections = [];
+    var sections = [], end;
     $('.section_row').each(function(i, row) {
       var title = $('input', row).val();
       var first = parseInt($('.start_page', row).val(), 10);
-      var last  = parseInt($('.end_page', row).val(), 10);
-      if (title) sections.push({title : title, start_page : first, end_page : last});
+      if (title) sections.push({title : title, start_page : first});
+    });
+    sections = _.sortBy(sections, function(section){ return -section.start_page; });
+    _.each(sections, function(section, i) {
+      section.end_page = end || currentDocument.api.numberOfPages();
+      end = section.start_page - 1;
     });
     return sections;
   },
@@ -64,7 +68,7 @@ dc.ui.SectionEditor = Backbone.View.extend({
     if (!_.any(this.sections)) return _.each(_.range(3), function(){ me.addRow(); });
     _.each(this.sections, function(sec) {
       var pages = sec.pages.split('-');
-      me.addRow({title : sec.title, start_page : parseInt(pages[0], 10), end_page : parseInt(pages[1], 10)});
+      me.addRow({title : sec.title, start_page : parseInt(pages[0], 10)});
     });
   },
 
