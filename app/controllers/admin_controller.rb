3aa277a2ac37b2bccb33bc8864444c41330e86dc
Jeremy Ashkenas <jashkenas@gmail.com>
2010-10-20 14:37:00 -0400

    Removing TODO.txt *sniff*

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -137,13 +137,6 @@ class AdminController < ApplicationController
     redirect_to '/'
   end
 
-  # Render the TODO.txt out as a web page.
-  def todo
-    @todo_text = File.read("#{Rails.root}/TODO")
-    @todo_text.gsub!(/^(\w+[^\n]+:)/, '</ul><h2>\1</h2><ul>').gsub!(/^\s+\*(.+?)\n\s*\n/m, '<li>\1</li>')
-    render :layout => false
-  end
-
   def test_exception_notifier
     1 / 0
   end
