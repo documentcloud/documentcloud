8598d8ac451984861d40bd8cf633c0dea1dd9526
Jeremy Ashkenas <jashkenas@gmail.com>
2010-11-01 15:51:29 -0400

    adding publish_at

---
+++ db/migrate/20101101192020_add_publish_at.rb
@@ -0,0 +1,9 @@
+class AddPublishAt < ActiveRecord::Migration
+  def self.up
+    add_column :documents, :publish_at, :timestamp
+  end
+
+  def self.down
+    remove_column :documents, :publish_at, :timestamp
+  end
+end
