85e4fa06429ebe3a5e1cd3b5b1c26bb0faa6f161
Jeremy Ashkenas <jashkenas@gmail.com>
2010-11-03 16:03:15 -0400

    removing unused bits of the placeholder patch.

--- public/javascripts/lib/jquery_extensions.js
+++ public/javascripts/lib/jquery_extensions.js
@@ -272,25 +272,24 @@
       });
       return ret;
     },
-    
+
     placeholder: function(opts) {
       var defaults = {
         message: '...',
-        className: 'placeholder',
-        clearClassName: 'show_cancel_search'
+        className: 'placeholder'
       };
       var options = $.extend({}, defaults, opts);
-    
+
       var setPlaceholder = function($input) {
         $input.val($input.attr('placeholder') || options.message);
         $input.addClass(options.className);
       };
-    
+
       return this.each(function() {
         var $this = $(this);
 
         if ($this.attr('type') == 'search') return;
-      
+
         $this.bind('blur', function() {
           if ($this.val() == '') {
             setPlaceholder($this);
@@ -300,20 +299,13 @@
             $this.val('');
           }
           $this.removeClass(options.className);
-        }).bind('keyup', function() {
-          var searchVal = $this.val();
-          if (searchVal != '' && searchVal != options.message) {
-            $this.parent().addClass(options.clearClassName);
-          } else {
-            $this.parent().removeClass(options.clearClassName);
-          }
         });
         _.defer(function(){
           $this.keyup().blur();
         });
       });
     }
-    
+
   });
 
   // Add mousewheel support:
