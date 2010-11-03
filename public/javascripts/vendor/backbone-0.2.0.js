088e13c74895739a146bdd59110bef60926bfdd6
Jeremy Ashkenas <jashkenas@gmail.com>
2010-11-03 12:10:59 -0400

    merging dxgriffiths' annotations to 'inherits'.

--- public/javascripts/vendor/backbone-0.2.0.js
+++ public/javascripts/vendor/backbone-0.2.0.js
@@ -734,23 +734,41 @@
   // Helpers
   // -------
 
+  // Shared empty constructor function to aid in prototype-chain creation.
+  var ctor = function(){};
+
   // Helper function to correctly set up the prototype chain, for subclasses.
   // Similar to `goog.inherits`, but uses a hash of prototype properties and
   // class properties to be extended.
-  var inherits = function(parent, protoProps, classProps) {
+  var inherits = function(parent, protoProps, staticProps) {
     var child;
+
+    // The constructor function for the new subclass is either defined by you
+    // (the "constructor" property in your `extend` definition), or defaulted
+    // by us to simply call `super()`.
     if (protoProps.hasOwnProperty('constructor')) {
       child = protoProps.constructor;
     } else {
       child = function(){ return parent.apply(this, arguments); };
     }
-    var ctor = function(){};
+
+    // Set the prototype chain to inherit from `parent`, without calling
+    // `parent`'s constructor function.
     ctor.prototype = parent.prototype;
     child.prototype = new ctor();
+
+    // Add prototype properties (instance properties) to the subclass.
     _.extend(child.prototype, protoProps);
-    if (classProps) _.extend(child, classProps);
+
+    // Add static properties to the constructor function, if supplied.
+    if (staticProps) _.extend(child, staticProps);
+
+    // Correctly set child's `prototype.constructor`, for `instanceof`.
     child.prototype.constructor = child;
+
+    // Set a convenience property in case the parent's prototype is needed later.
     child.__super__ = parent.prototype;
+
     return child;
   };
 
