c669ce92a9ff8eb93dc836f11094ac72abf6a1cb
Jeremy Ashkenas <jashkenas@gmail.com>
2010-11-19 20:25:27 -0500

    setting log_level to info temporarily.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
 # See everything in the log (default is :info)
-config.log_level = :warn
+config.log_level = :info
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
