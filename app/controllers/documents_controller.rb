a5fc521261a956dd7db12cceed68526006dffa9c
Jeremy Ashkenas <jashkenas@gmail.com>
2010-12-08 10:04:23 -0500

    trying to fix stale safari caching.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -152,6 +152,7 @@ class DocumentsController < ApplicationController
   def send_page_image
     return not_found unless current_page
     size = params[:page_name][SIZE_EXTRACTOR, 1]
+    @headers["Cache-Control"] = "no-store"
     redirect_to(current_page.authorized_image_url(size))
   end
 
