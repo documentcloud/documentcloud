1b9f60169c49e68e869a4e5a6a3f352674dcd3f3
Jeremy Ashkenas <jashkenas@gmail.com>
2011-01-04 09:53:49 -0500

    with a working search box, starting on numbers.

---
+++ public/javascripts/home/home.js
@@ -0,0 +1,44 @@
+$(function() {
+
+  window.HomePage = Backbone.View.extend({
+
+    el : document.body,
+
+    events : {
+      'keydown #search_box':  'maybeSearch',
+      'search #search_box':   'search',
+      'focus #search_box':    'addFocus',
+      'blur #search_box':     'removeFocus',
+      'click .cancel_search': 'cancelSearch'
+    },
+
+    initialize : function() {
+      this.box = $('#search_box');
+    },
+
+    search : function() {
+      var query = this.box.val();
+      if (query) window.location = '/public/#search/' + encodeURIComponent(query);
+    },
+
+    cancelSearch : function() {
+      this.box.val('');
+    },
+
+    maybeSearch : function(e) {
+      if (e.keyCode == 13) this.search();
+    },
+
+    addFocus : function() {
+      $('#search_box_wrapper').addClass('focus');
+    },
+
+    removeFocus : function() {
+      $('#search_box_wrapper').removeClass('focus');
+    }
+
+  });
+
+  window.homepage = new HomePage();
+
+});
