1427613bf9b623418eb917170bc8f820b96b54a0
Jeremy Ashkenas <jashkenas@gmail.com>
2011-01-09 16:30:13 -0500

    Pushing ahead with a big refactor + internal pages.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,24 @@ class HomeController < ApplicationController
   include DC::Access
 
   def index
-    @document = Document.random(:conditions => {:access => PUBLIC}, :limit => 1).first
+    @documents = Document.random(:conditions => {:access => PUBLIC}, :limit => 1)
+  end
+
+  def opensource
+    yaml    = yaml_for('opensource')
+    @news   = yaml['news']
+    @github = yaml['github']
+  end
+
+
+  private
+
+  def date_sorted(list)
+    list.sort_by {|item| item.last }.reverse
+  end
+
+  def yaml_for(action)
+    YAML.load_file("#{Rails.root}/app/views/static/#{action}.yml")
   end
 
 end
