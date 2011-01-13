bcbcca13e56103c260001bdbcec6d82303084b14
Jeremy Ashkenas <jashkenas@gmail.com>
2011-01-13 16:06:17 -0500

    don't try to migrate all at once.

--- db/migrate/20110113204915_store_hit_count_on_documents.rb
+++ db/migrate/20110113204915_store_hit_count_on_documents.rb
@@ -2,11 +2,6 @@ class StoreHitCountOnDocuments < ActiveRecord::Migration
   def self.up
     add_column 'documents', 'hit_count', :integer, :null => false, :default => 0
     add_index 'documents', ['hit_count'], :name => 'index_documents_on_hit_count'
-
-    # Initial hit counts.
-    Document.find_each do |doc|
-      doc.update_attributes :hit_count => doc.remote_urls.sum(:hits)
-    end
   end
 
   def self.down
