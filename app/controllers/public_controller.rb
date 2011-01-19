feabfe831a240936fab690541551556d2fe23bb8
Jeremy Ashkenas <jashkenas@gmail.com>
2011-01-19 14:49:01 -0500

    adding analytics to /public

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -7,6 +7,7 @@ class PublicController < ApplicationController
     return redirect_to("/") if logged_in?
     return redirect_to("/public/") unless request.path.match(/\/$/)
     @organizations = Organization.listed
+    @include_analytics = true
     render :template => 'workspace/index', :layout => 'workspace'
   end
 
