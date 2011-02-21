ddf3de49df4d281c70cec9f85c63add5927bd9b1
Jeremy Ashkenas <jashkenas@gmail.com>
2011-02-21 10:16:44 -0500

    trying port 25 for dreamhost

--- config/initializers/smtp_configuration.rb
+++ config/initializers/smtp_configuration.rb
@@ -3,7 +3,7 @@ unless Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'mail.documentcloud.org',
-    :port           => 26,
+    :port           => 25,
     :user_name      => 'smtp@documentcloud.org',
     :password       => SECRETS['smtp_password'],
     :authentication => :plain
