3a1e43e58e2e17f3e3ec642ef615bad8e9202403
Jeremy Ashkenas <jashkenas@gmail.com>
2011-03-01 09:48:40 -0500

    gotta commit if you want to deploy.

--- config/initializers/misc_constants.rb
+++ config/initializers/misc_constants.rb
@@ -5,5 +5,5 @@
 require 'erb'
 
 ANALYTICS_DB = YAML.load(ERB.new(File.read("#{Rails.root}/config/database_analytics.yml")).result(binding))[Rails.env]
-MAIN_DB = YAML.load((ERB.new(File.read("#{Rails.root}/config/database.yml")).result(binding))[Rails.env]
+MAIN_DB      = YAML.load(ERB.new(File.read("#{Rails.root}/config/database.yml")).result(binding))[Rails.env]
 require 'lib/dc'
