8acadb073049a9ea67a77c213a68806f21cf3c04
Jeremy Ashkenas <jashkenas@gmail.com>
2011-03-01 09:47:20 -0500

    gotta commit if you want to deploy.

--- config/initializers/misc_constants.rb
+++ config/initializers/misc_constants.rb
@@ -2,6 +2,8 @@
 # /lib folder code will be refreshed for each request.
 # ActiveSupport::Dependencies.explicitly_unloadable_constants << 'DC'
 
-ANALYTICS_DB = YAML.load_file("#{Rails.root}/config/database_analytics.yml")[Rails.env]
-MAIN_DB = YAML.load_file("#{Rails.root}/config/database.yml")[Rails.env]
+require 'erb'
+
+ANALYTICS_DB = YAML.load(ERB.new(File.read("#{Rails.root}/config/database_analytics.yml")).result(binding))[Rails.env]
+MAIN_DB = YAML.load((ERB.new(File.read("#{Rails.root}/config/database.yml")).result(binding))[Rails.env]
 require 'lib/dc'
