978a28d91ccde94cb2ba621926c4e3789f6c0ecd
Jeremy Ashkenas <jashkenas@gmail.com>
2011-03-09 13:34:10 -0500

    revise migration.

--- db/migrate/20101214171909_count_reviewers_on_documents.rb
+++ db/migrate/20101214171909_count_reviewers_on_documents.rb
@@ -1,7 +1,6 @@
 class CountReviewersOnDocuments < ActiveRecord::Migration
   def self.up
     add_column  :documents, :document_reviewers_count, :integer, :default => 0, :null => false
-    Document.all.each {|d| Document.update_counters d.id, :document_reviewers_count => d.reviewers.count }
   end
 
   def self.down
