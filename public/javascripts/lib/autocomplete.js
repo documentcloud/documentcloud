5358761101a3cdbf05750a4804812d56c92681c3
Samuel Clay <samuel@ofbrooklyn.com>
2011-03-23 12:38:55 -0400

    Fixing stubbed in autocomplete view.

--- public/javascripts/lib/autocomplete.js
+++ public/javascripts/lib/autocomplete.js
@@ -1,7 +1,7 @@
 dc.ui.Autocomplete = Backbone.View.extend({
   
   initialize : function() {
-    console.log(['options', this.options, el]);
+    console.log(['options', this.options, this.el]);
   }
   
   
