3fef496e983e0103ffb64a546e1c817d49e3920d
Jeremy Ashkenas <jashkenas@gmail.com>
2011-03-29 10:15:29 -0400

    idiot-proofing the CloudCrowdImporter a bit.

--- lib/dc/import/cloud_crowd_importer.rb
+++ lib/dc/import/cloud_crowd_importer.rb
@@ -2,20 +2,22 @@ module DC
   module Import
 
     class CloudCrowdImporter
+      include DC::Access
 
       DEFAULT_OPTIONS = {
         'organization_id'   => 0,
         'account_id'        => 0,
         'source'            => 'Unknown',
-        'access'            => DC::Access::PRIVATE
+        'access'            => PRIVATE
       }
 
       def import(urls, options={}, large=false)
-        options = options.stringify_keys
+        options = DEFAULT_OPTIONS.merge options.stringify_keys
+        options['access'] = PRIVATE if options['access'] == PENDING
         RestClient.post(DC_CONFIG['cloud_crowd_server'] + '/jobs', {:job => {
           'action'  => large ? 'large_document_import' : 'document_import',
           'inputs'  => urls,
-          'options' => DEFAULT_OPTIONS.merge(options),
+          'options' => options,
           'callback_url' => "#{DC.server_root(:ssl => false)}/import/cloud_crowd"
         }.to_json})
       end
