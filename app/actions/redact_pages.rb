23041241093fa42abb7e848ab70d0d6c5d2671e1
Jeremy Ashkenas <jashkenas@gmail.com>
2011-04-05 14:11:45 -0400

    Redactions, version 0.1

--- app/actions/redact_pages.rb
+++ app/actions/redact_pages.rb
@@ -7,7 +7,7 @@ class RedactPages < DocumentModBase
   # The zoom ratio at which we'll be drawing redactions.
   ENLARGEMENT = 1000.0 / 700.0
 
-  GM_ARGS = "-density 150 -limit memory 256MiB -limit map 512MiB"
+  GM_ARGS = '-density 150 -limit memory 256MiB -limit map 512MiB'
 
   def process
     begin
@@ -25,23 +25,28 @@ class RedactPages < DocumentModBase
   private
 
   def redact
+    @page_text = {}
     Docsplit.extract_pages @pdf
     FileUtils.rm @pdf
     redactions_by_page = options['redactions'].group_by {|r| r['page'] }
     redactions_by_page.each {|page, redactions| redact_page page, redactions }
+    rebuild_pdf
+    rebuild_text
     document.reindex_all! access
   end
 
   def redact_page(page, redactions)
+    base = "#{document.slug}_#{page}"
 
     # Make the list of image file paths.
     images = {}
     Page::IMAGE_SIZES.each do |size, geometry|
-      images[size] = "#{document.slug}_#{page}_#{size}.gif"
+      images[size] = "#{base}_#{size}.gif"
     end
 
     # Get the large version of the page image.
-    page_pdf_path = "#{document.slug}_#{page}.pdf"
+    page_pdf_path  = "#{base}.pdf"
+    page_tiff_path = "#{base}.tif"
     File.open(images['large'], 'w+') do |f|
       f.write(asset_store.read(document.page_image_path(page, 'large')))
     end
@@ -67,6 +72,31 @@ class RedactPages < DocumentModBase
     # Save the redacted images to our asset store.
     asset_store.save_page_images(document, page, images, access)
 
+    # Write out the new redacted pdf page, and tiff version for OCR.
+    `gm convert #{GM_ARGS} #{images['large']} #{page_pdf_path} 2>&1`
+    `gm convert #{GM_ARGS} -density 200x200 -colorspace GRAY #{images['large']} #{page_tiff_path} 2>&1`
+
+    # OCR the large version of the image.
+    `tesseract #{page_tiff_path} #{base} -l eng 2>&1`
+    @page_text[page] = Docsplit.clean_text(DC::Import::Utils.read_ascii("#{base}.txt"))
+
+  end
+
+  # Create the new PDF for the full document, and save it to the asset store.
+  def rebuild_pdf
+    page_paths = (1..document.page_count).map {|i| "#{document.slug}_#{i}.pdf" }
+    `pdftk #{page_paths.join(' ')} cat output #{@pdf}`
+    asset_store.save_pdf(document, @pdf, access)
+  end
+
+  # Create all the new Page models, for pages that have been changed.
+  def rebuild_text
+    page_numbers = @page_text.keys
+    Page.destroy_all "document_id = #{document.id} and page_number in (#{page_numbers.join(',')})"
+    rows = @page_text.map do |pair|
+      "(#{document.organization_id}, #{document.account_id}, #{document.id}, #{access}, #{pair[0]}, '#{PGconn.escape(pair[1])}')"
+    end
+    Page.connection.execute "insert into pages (organization_id, account_id, document_id, access, page_number, text) values #{rows.join(",\n")};"
   end
 
 end
