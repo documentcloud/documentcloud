450e48f6903149857aa8324e380b723022058a07
Jeremy Ashkenas <jashkenas@gmail.com>
2011-04-06 12:31:43 -0400

    removing the redacted bit.

--- db/migrate/20110406140834_add_redacted_flag_to_pages.rb
+++
@@ -1,9 +0,0 @@
-class AddRedactedFlagToPages < ActiveRecord::Migration
-  def self.up
-    add_column "pages", "redacted", :boolean, :default => false, :null => false
-  end
-
-  def self.down
-    remove_column "pages", "redacted"
-  end
-end
