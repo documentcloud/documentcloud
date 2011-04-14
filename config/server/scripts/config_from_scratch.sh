14e8f4ab0e047a574cc15c08ad53c580968ffe88
Jeremy Ashkenas <jashkenas@gmail.com>
2011-04-14 13:47:53 -0400

    mo modern config

--- config/server/scripts/config_from_scratch.sh
+++ config/server/scripts/config_from_scratch.sh
@@ -16,6 +16,9 @@ deb http://security.ubuntu.com/ubuntu karmic-security main universe multiverse
 deb-src http://security.ubuntu.com/ubuntu karmic-security main universe
 EOF
 
+add-apt-repository "deb http://archive.canonical.com/ lucid partner"
+add-apt-repository ppa:pitti/postgresql
+
 apt-get update
 apt-get -y upgrade
 
@@ -24,38 +27,41 @@ export DEBIAN_FRONTEND=readline
 yes yes| apt-get install -y sun-java6-bin
 export DEBIAN_FRONTEND=noninteractive
 
-PACKAGES='build-essential postgresql postgresql-client
-postgresql-contrib libpq-dev git-core mercurial scons libexpat1-dev
+PACKAGES='build-essential postgresql-9.0 postgresql-client-9.0
+postgresql-contrib-9.0 libpq-dev git-core mercurial scons libexpat1-dev
 libxml2-dev libxslt-dev postfix ruby ri rdoc irb ruby1.8-dev
 zlib1g-dev libzlib-ruby sqlite3 libsqlite3-dev libcurl4-dev
 checkinstall libbz2-dev graphicsmagick pdftk xpdf libitext-java
 openoffice.org openoffice.org-java-common libtiff4-dev libpng12-dev
 libjpeg62-dev libleptonica-dev tesseract-ocr-dev tesseract-ocr-eng
-xfsprogs libpcre3-dev munin munin-node emacs23-nox lzop sysstat
-libopenssl-ruby sunspot sunspot_ruby'
+xfsprogs libpcre3-dev lzop sysstat libopenssl-ruby'
 
 echo $PACKAGES | xargs apt-get install -y
 
-GEMS_VERSION=1.3.5
+GEMS_VERSION=1.7.2
 gem --version 2>/dev/null | grep -q $GEMS_VERSION || {
   cd /tmp
-  wget http://rubyforge.org/frs/download.php/60718/rubygems-$GEMS_VERSION.tgz
+  wget http://production.cf.rubygems.org/rubygems/rubygems-$GEMS_VERSION.tgz
   tar xzvf rubygems-$GEMS_VERSION.tgz
   ruby rubygems-$GEMS_VERSION/setup.rb
   ln -s /usr/bin/gem1.8 /usr/local/bin/gem
   rm -rf rubygems-$GEMS_VERSION*
 }
 
-GEMS='pg sqlite3-ruby rails passenger sinatra right_aws
+GEMS='pg sqlite3 passenger sinatra right_aws fastercsv sanitize
 rest-client rack bcrypt-ruby rdiscount rubyzip libxml-ruby nokogiri json hpricot
-calais curb daemons cloud-crowd yui-compressor jammit docsplit'
+calais curb daemons cloud-crowd yui-compressor jammit docsplit sunspot sunspot_rails'
 
 echo $GEMS | xargs gem install --no-ri --no-rdoc
+
+gem install rails --no-ri --no-rdoc --version 2.3.11
 
 cd /home/$USERNAME
 
 # approve github ssh host key
 grep -q github .ssh/known_hosts 2>/dev/null || ssh-keyscan -t rsa github.com > .ssh/known_hosts
+
+# [copy over the secrets directory].
 
 chown -R $USERNAME .
 test -e document-cloud || sudo -u $USERNAME git clone git@github.com:documentcloud/document-cloud.git document-cloud
@@ -85,15 +91,15 @@ uname -a | tee -a /etc/motd
 perl -pi -e 's/smtpd_use_tls=yes/smtpd_use_tls=no/' /etc/postfix/main.cf
 
 # setup dummy postgres environment so that you can verify rails is working
-cp config/server/postgres/pg_hba.conf  /etc/postgresql/8.4/main/pg_hba.conf
-/etc/init.d/postgresql-8.4 reload
+cp config/server/postgres/pg_hba.conf  /etc/postgresql/9.0/main/pg_hba.conf
+/etc/init.d/postgresql reload
 sudo -u postgres createuser -s ubuntu
 sudo -u postgres createuser -s documentcloud
 sudo -u postgres psql -c "alter user documentcloud password 'YOUR_DB_PASSWORD_GOES_HERE' "
 sudo -u postgres createdb dcloud_$RAILS_ENVIRONMENT
 cd /home/$USERNAME/document-cloud
 sudo -u postgres psql -f db/development_structure.sql dcloud_$RAILS_ENVIRONMENT 2>&1|grep ERROR
-#rake $RAILS_ENVIRONMENT db:migrate
+# rake $RAILS_ENVIRONMENT db:migrate
 
 # nginx
 test -e /usr/local/nginx || /usr/bin/passenger-install-nginx-module --auto --auto-download \
@@ -103,20 +109,11 @@ grep -q "$LINE" .bashrc 2>/dev/null || echo "$LINE" >> .bashrc
 mkdir -p /usr/local/nginx/conf/sites-enabled /var/log/nginx/
 mkdir -p /var/log/nginx
 cp config/server/nginx/nginx.conf /usr/local/nginx/conf/
-cp config/server/nginx/{$RAILS_ENVIRONMENT,munin}.conf /usr/local/nginx/conf/sites-enabled/
+cp config/server/nginx/$RAILS_ENVIRONMENT.conf /usr/local/nginx/conf/sites-enabled/
 # TODO nginx configuration is not rock solid
 cp config/server/nginx/nginx.init /etc/init.d/nginx
 update-rc.d nginx defaults
 /etc/init.d/nginx start
-
-# munin
-cp -v config/server/munin/* /etc/munin/
-sudo cp -v config/server/munin/plugins/* /usr/share/munin/plugins/
-for f in config/server/munin/plugins/*; do
-    ln -v -sf /usr/share/munin/plugins/$(basename $f) /etc/munin/plugins
-done
-for f in /etc/munin/plugins/{if_err_eth0,postfix_mailqueue,postfix_mailvolume,entropy}; do test -e $f && rm -v $f; done
-/etc/init.d/munin-node restart
 
 # TODO configure cloud-crowd
 
