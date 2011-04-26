00e6cdd1e873aa0036a35206f2eab5da4df5cc79
Amanda Hickman <amanda@documentcloud.org>
2011-04-26 16:49:36 -0400

    Final tweak, "has been" not "is" Closes GH-168.

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -3,7 +3,7 @@ class CollaboratorsController < ApplicationController
   def create
     account = Account.lookup(pick(params, :email)[:email])
     return json(nil, 404) if !account || account.id == current_account.id
-    return json({:errors => ['That account is disabled.']}, 409) if account.role == Account::DISABLED
+    return json({:errors => ['That account has been disabled.']}, 409) if account.role == Account::DISABLED
     success = current_project.add_collaborator account
     return json(account, 409) unless success
     json account.to_json(:include_organization => true)
