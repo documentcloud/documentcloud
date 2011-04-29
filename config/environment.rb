0d424c31e9d6ff0a5e9692c76687e18b713f4b0b
Jeremy Ashkenas <jashkenas@gmail.com>
2011-04-29 10:46:49 -0400

    upgrading sunspot to 1.2.1

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'pg',                    :version => '>= 0.9.0'
   config.gem 'jammit',                :version => '>= 0.5.0'
   config.gem 'docsplit',              :version => '>= 0.5.0'
-  config.gem 'sunspot_rails',         :version => '>= 1.1.0',       :lib => 'sunspot/rails'
+  config.gem 'sunspot_rails',         :version => '>= 1.2.1'
   config.gem 'rdiscount',             :version => '>= 1.6.5'
   config.gem 'fastercsv',             :version => '>= 1.5.3'
   config.gem 'sanitize',              :version => '>= 2.0.0'
