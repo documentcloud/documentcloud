6b45a02ba5723313063da617bb48b10f7fdeebf0
Jeremy Ashkenas <jashkenas@gmail.com>
2011-06-24 10:08:24 -0400

    show and hide the spinner while trying to download.

--- public/javascripts/app/downloader.js
+++ public/javascripts/app/downloader.js
@@ -1,11 +1,13 @@
 // A simple iframe-based downloader that can fire a callback when the download
 // has begun.
 dc.app.download = function(url, callback) {
+  dc.ui.spinner.show();
   var iframe = document.createElement('iframe');
   iframe.src = url;
   iframe.style.display = 'none';
   var timer = setInterval(function(){
     if (iframe.contentDocument.readyState == 'complete') {
+      dc.ui.spinner.hide();
       clearInterval(timer);
       if (callback) callback();
       $(iframe).remove();
