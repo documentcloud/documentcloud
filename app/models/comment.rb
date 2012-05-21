d20f3d14b61033218bd972151c93906c9ea4d767
Ted Han <ted@knowtheory.net>
2012-05-20 19:09:40 -0500

    Adding comment model, styled after annotation model.

---
+++ app/models/comment.rb
@@ -0,0 +1,25 @@
+class Comment < ActiveRecord::Base
+  include DC::Access
+  
+  belongs_to :annotation
+  belongs_to :document
+  belongs_to :commenter
+
+  attr_accessor :author
+  
+  validates_presence_of :text
+  
+  def canonical(options = {})
+    data           = {
+      'text'       => text
+      'created_at' => created_at
+    }
+    data.merge!({ 'author' => author.to_json(options) }) if author
+    data
+  end
+
+  def to_json(options={})
+    canonical.to_json
+  end
+  
+end
