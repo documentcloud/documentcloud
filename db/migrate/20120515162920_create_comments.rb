4a81385508b58d0abc3c774bb5717aa39be8477d
Ted Han <ted@knowtheory.net>
2012-05-20 19:08:09 -0500

    Adding a comments table.

---
+++ db/migrate/20120515162920_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def self.up
+    create_table :comments do |t|
+      t.text "text", :null => false
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :comments
+  end
+end
