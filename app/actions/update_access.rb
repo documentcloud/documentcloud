2ad209b9459b8c928f9ed51df2ca81bc2f66ccba
Ted Han <ted@knowtheory.net>
2012-08-27 16:40:53 -0500

    Missing files should never block an access update.

--- app/actions/update_access.rb
+++ app/actions/update_access.rb
@@ -13,7 +13,11 @@ class UpdateAccess < CloudCrowd::Action
       Page.update_all(*sql)
       Entity.update_all(*sql)
       EntityDate.update_all(*sql)
-      DC::Store::AssetStore.new.set_access(document, access)
+      begin
+        DC::Store::AssetStore.new.set_access(document, access)
+      rescue RightAws::AwsError => e
+        raise e unless http_code == "404"
+      end
       document.update_attributes(:access => access)
     rescue Exception => e
       LifecycleMailer.deliver_exception_notification(e, options)
