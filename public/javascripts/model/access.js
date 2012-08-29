3199c367805c575c0f321fa91ed7d6880278475b
Ted Han <ted@knowtheory.net>
2012-08-29 10:54:49 -0500

    Updating access descriptions and adding a moderated level.

--- public/javascripts/model/access.js
+++ public/javascripts/model/access.js
@@ -2,14 +2,15 @@
 
 dc.access = {
 
-  DELETED       : 0,   // The document was deleted, and will be removed soon.
-  PRIVATE       : 1,   // The document is only visible to it's owner.
+  DELETED       : 0,   // The resource was deleted, and will be removed soon.
+  PRIVATE       : 1,   // The resource is only visible to it's owner.
   ORGANIZATION  : 2,   // Visible to both the owner and her organization.
   EXCLUSIVE     : 3,   // Published, but exclusive to the owner's organization.
   PUBLIC        : 4,   // Free and public to all.
-  PENDING       : 5,   // The document is being processed (acts as disabled).
-  INVISIBLE     : 6,   // The document has been taken down (perhaps temporary).
-  ERROR         : 7,   // The document is broken, or failed to import.
+  PENDING       : 5,   // The resource is being processed (acts as disabled).
+  INVISIBLE     : 6,   // The resource has been taken down (perhaps temporary).
+  ERROR         : 7,   // The resource is broken, or failed to import.
+  MODERATED     : 8,   // The resource has been flagged by moderators
 
   // The inverse mapping, from access levels back to strings.
   NAMES : {
