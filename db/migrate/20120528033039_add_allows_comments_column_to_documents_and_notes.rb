43c88c613f6c210f0babe4823f682895281933fa
Ted Han <ted@knowtheory.net>
2012-10-30 03:12:42 -0500

    set defaults for comment_access.

--- db/migrate/20120528033039_add_allows_comments_column_to_documents_and_notes.rb
+++ db/migrate/20120528033039_add_allows_comments_column_to_documents_and_notes.rb
@@ -1,7 +1,18 @@
 class AddAllowsCommentsColumnToDocumentsAndNotes < ActiveRecord::Migration
   def self.up
-    add_column :documents,   :comment_access, :integer, :null => false
-    add_column :annotations, :comment_access, :integer, :null => false
+    add_column :documents,      :comment_access, :integer
+    add_column :annotations,    :comment_access, :integer
+    
+    Document.find_each do |document|
+      document.update_attribute(:comment_access, document.access)
+    end
+    
+    Annotation.find_each do |note|
+      note.update_attribute(:comment_access, note.access)
+    end
+    
+    change_column :documents,   :comment_access, :integer, :null => true
+    change_column :annotations, :comment_access, :integer, :null => true
   end
 
   def self.down
