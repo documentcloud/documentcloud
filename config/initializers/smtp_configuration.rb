a17614d044b296422ebb651f41ec76421e5e0b4c
Ted Han <ted@knowtheory.net>
2012-12-10 11:48:09 -0600

    Prepare for migration away from dreamhost smtp

--- config/initializers/smtp_configuration.rb
+++ config/initializers/smtp_configuration.rb
@@ -2,12 +2,13 @@ unless Rails.env.development?
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => SECRETS['smtp_host'],
-    :port           => 25,
-    :domain         => SECRETS['smtp_domain'],
-    :user_name      => SECRETS['smtp_user'],
-    :password       => SECRETS['smtp_password'],
-    :authentication => :login
+    :address              => SECRETS['smtp_host'],
+    :port                 => 25,
+    :domain               => SECRETS['smtp_domain'],
+    :user_name            => SECRETS['smtp_user'],
+    :password             => SECRETS['smtp_password'],
+    :authentication       => :login,
+    :enable_starttls_auto => true
   }
 
 end
