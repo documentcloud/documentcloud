7645d55520c9de2d78863a07fddba510c1f3e7a5
Ted Han <ted@knowtheory.net>
2013-03-05 10:56:07 -0600

    Fix public search bug for logged in users.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,7 +3,14 @@ class PublicController < ApplicationController
   # Public search.
   def index
     @organizations = Organization.listed
-    @current_account = current_account
+    # commented out for now, because the workspace determines
+    # whether it's in public search solely based on the presence
+    # of dc.account
+    # 
+    # as a consequence, when in the public search, logged in users
+    # will not be able to tell from the login links that they are
+    # logged in.
+    #@current_account = current_account
     @include_analytics = true
     render :template => 'workspace/index', :layout => 'workspace'
   end
