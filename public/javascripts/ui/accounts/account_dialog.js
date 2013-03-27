de18d703fabd968627035ebff641886d36b138d3
Nathan Stitt <nathan@stitt.org>
2013-03-27 16:52:19 -0500

    Organization Settings
    Add a language selection sheet to accounts dialog and re-purpose it as
    organizations settings dialog, with accounts at bottom.
    
    Language list will be further refined.

--- public/javascripts/ui/accounts/account_dialog.js
+++ public/javascripts/ui/accounts/account_dialog.js
@@ -6,13 +6,17 @@ dc.ui.AccountDialog = dc.ui.Dialog.extend({
 
   events : {
     'click .ok'           : 'close',
-    'click .new_account'  : 'newAccount'
+    'click .new_account'  : 'newAccount',
+    'click .language_sheet .chooser'  : 'toggleChooseLanguage',
+    'click .language_sheet td' : 'chooseLanguage'
   },
 
   constructor : function() {
     dc.ui.Dialog.call(this, {
       mode          : 'custom',
-      title         : 'Manage Accounts: ' + dc.account.organization().get('name'),
+      title         : 'Manage ' + ( Accounts.current().isAdmin() ?
+                                    'Organization: ' + dc.account.organization().get('name') : 
+                                    'Account' ),
       information   : 'group: ' + dc.account.organization().get('slug')
     });
     Accounts.bind('reset', _.bind(this._renderAccounts, this));
@@ -27,7 +31,11 @@ dc.ui.AccountDialog = dc.ui.Dialog.extend({
     this._container = this.$('.custom');
     this._container.setMode('not', 'draggable');
     this._container.html(JST['account/dialog']({}));
-    if (Accounts.current().isAdmin()) this.addControl(this.make('div', {'class': 'minibutton dark new_account', style : 'width: 90px;'}, 'New Account'));
+    if (Accounts.current().isAdmin()){
+      this._container.prepend( JST['account/organization_settings_dialog']({ organization: dc.account.organization().toJSON() }) );
+      this.addControl(this.make('div', {'class': 'minibutton dark new_account', style : 'width: 90px;'}, 'New Account'));
+
+    }
     this.list = this.$('.account_list_content');
     this._renderAccounts();
     return this;
@@ -49,6 +57,22 @@ dc.ui.AccountDialog = dc.ui.Dialog.extend({
     $(this.el).hide();
     $(document.body).removeClass('overlay');
     this._open = false;
+  },
+
+  chooseLanguage: function(ev){
+    target = $(ev.target);
+    target.closest('table').find('td').removeClass('active');
+    target.addClass('active');
+    // FIXME - remove & replace with listen action on model
+    // once that's hooked up
+    target.closest('.language_sheet').find('.current').html( target.attr('data-lang') );
+    _.delay( function(){
+      target.closest('.language_sheet').removeClass('open');
+    }, 500);  // wait a bit so they can see that the languages was chosen
+  },
+
+  toggleChooseLanguage: function(){
+    this.$('.language_sheet').toggleClass('open')
   },
 
   isOpen : function() {
