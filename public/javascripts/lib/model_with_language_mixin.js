a91bcae4068871807a44ecfd8cb2814a63a1af43
Nathan Stitt <nathan@stitt.org>
2013-04-03 13:27:34 -0500

    Data for model's language mixin from dc.languages

--- public/javascripts/lib/model_with_language_mixin.js
+++ public/javascripts/lib/model_with_language_mixin.js
@@ -1,34 +1,11 @@
 window.ModelWithLanguageMixin = {
 
-  LANGUAGE_CHOICES: {
-      en: 'English',
-      es: 'Spanish',
-      hi: 'Hindi',
-      pt: 'Portuguese',
-      ru: 'Russian',
-      ja: 'Japanese',
-      de: 'German',
-      id: 'Malay/Indonesian',
-      vi: 'Vietnamese',
-      ko: 'Korean',
-      fr: 'French',
-      fa: 'Persian',
-      tk: 'Turkish',
-      it: 'Italian',
-      no: 'Norwegian',
-      pl: 'Polish',
-      chi: 'Chinese (simplified)',
-      zho: 'Chinese (traditional)',
-      ar: 'Aribic',
-      th: 'Thai'
-  },
-
   getLanguageCode: function(){
-    return this.get('language') || 'en';
+    return this.get('language') || 'eng';
   },
 
   getLanguageName: function(){
-    return this.LANGUAGE_CHOICES[ this.getLanguageCode() ];
+    return dc.language.NAMES[ this.getLanguageCode() ];
   },
 
   getLanguage: function(){
