65a14f53c765f19a63cec46a45335224561542e5
Nathan Stitt <nathan@stitt.org>
2013-04-03 13:32:32 -0500

    Modify server-side to match our testing languages

--- lib/dc/language.rb
+++ lib/dc/language.rb
@@ -1,11 +1,30 @@
 module DC
   # The official list of supported languages
+
+  # should we be using iso15924 script codes?
+  #   http://www.unicode.org/iso15924/iso15924-codes.html
+
   module Language
-    SUPPORTED = ['en', 'es', 'fr']
-    ALPHA3 = {
-      'en' => 'eng',
-      'es' => 'spa',
-      'fr' => 'fra'
+    SUPPORTED = [
+                 'eng', 'spa', 'fra','nor','swe','ara','deu',
+                 'chi','zho','jpn','hin','rus'
+               ]
+
+    NAMES = {
+      'eng' => 'English',
+      'spa' => 'Spanish',
+      'fra' => 'French',
+      'nor' => 'Norwegian',
+      'swe' => 'Swedish',
+      'ara' => 'Arabic',
+      'deu' => 'German',
+      'chi' => 'Chinese(Tranditional)',
+      'zho' => 'Chinese(Simplified)',
+      'jpn' => 'Japanese',
+      'hin' => 'Hindu',
+      'rus' => 'Russian'
     }
+
   end
+
 end
