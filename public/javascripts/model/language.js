801c5a977ce0650c27fc8b35532ac6511a58e7b3
Nathan Stitt <nathan@stitt.org>
2013-04-03 13:25:01 -0500

    Create JS version of lib/dc/languages.

---
+++ public/javascripts/model/language.js
@@ -0,0 +1,27 @@
+// Keep this file in sync with lib/dc/language.rb
+
+
+
+dc.language = {
+
+  SUPPORTED: [
+    'eng', 'spa', 'fra','nor','swe','ara','deu',
+    'chi','zho','jpn','hin','rus'
+  ],
+
+  NAMES: {
+    'eng': 'English',
+    'spa': 'Spanish',
+    'fra': 'French',
+    'nor': 'Norwegian',
+    'swe': 'Swedish',
+    'ara': 'Arabic',
+    'deu': 'German',
+    'chi': 'Chinese(Tranditional)',
+    'zho': 'Chinese(Simplified)',
+    'jpn': 'Japanese',
+    'hin': 'Hindu',
+    'rus': 'Russian'
+  }
+
+};
