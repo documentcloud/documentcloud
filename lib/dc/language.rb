69c7ccb7b00cfd3d9ed7b0349581b0ffe19bb9ef
Ted Han <ted@knowtheory.net>
2013-04-16 18:21:18 -0500

    Add German to documentcloud.

--- lib/dc/language.rb
+++ lib/dc/language.rb
@@ -1,11 +1,12 @@
 module DC
   # The official list of supported languages
   module Language
-    SUPPORTED = ['en', 'es', 'fr']
+    SUPPORTED = ['en', 'es', 'fr', 'de']
     ALPHA3 = {
       'en' => 'eng',
       'es' => 'spa',
-      'fr' => 'fra'
+      'fr' => 'fra',
+      'de' => 'deu'
     }
   end
 end
