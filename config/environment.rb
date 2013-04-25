b2260203e3c98053f44d32ed98f3957b4fe2e676
Nathan Stitt <nathan@stitt.org>
2013-04-25 15:31:51 -0500

    bump version to latest

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require 'erb'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
