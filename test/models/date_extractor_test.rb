86cd3a1ea4e07cfe735ee9eb6a50566e9ae154b6
Nathan Stitt <nathan@stitt.org>
2013-07-18 12:18:07 -0500

    DateExtractor recognize both US and international
    This normalizes the date to YYYY-MM-DD if the date is all numeric and
    the extractor is in American mode.

--- test/models/date_extractor_test.rb
+++ test/models/date_extractor_test.rb
@@ -15,11 +15,23 @@ class DateExtractorTest < ActiveSupport::TestCase
   DATES = [Date.parse('2004-12-10'), Date.parse('1419-1-2')]
 
   it "correctly extracts dates from text" do
-    skip("Need to decide on Date.parse behavior for American dates")
-    dates = DC::Import::DateExtractor.new.extract_dates(DOC).map {|d| d[:date] }
+    dates = DC::Import::DateExtractor.american.extract_dates(DOC).map {|d| d[:date] }
     assert_equal dates.sort, DATES.sort
   end
 
+  it "parses US dates" do
+    parser = DC::Import::DateExtractor.american
+    assert_equal Date.new(2010,10,30), parser.extract_dates('10-30-2010').first[:date]
+    assert_equal Date.new(2010,12,30), parser.extract_dates('2010-12-30').first[:date]
+    assert_equal Date.new(2010,5,6), parser.extract_dates('5/6/2010').first[:date]
+  end
+
+  it "parses international dates" do
+    parser = DC::Import::DateExtractor.international
+    assert_equal Date.new(2010,10,30), parser.extract_dates('30-10-2010').first[:date]
+    assert_equal Date.new(2010,12,30), parser.extract_dates('2010-12-30').first[:date]
+    assert_equal Date.new(2010,5,6), parser.extract_dates('6/5/2010').first[:date]
+  end
 
 
 end
