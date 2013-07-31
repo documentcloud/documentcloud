1f9a243441d854a005d8b394379aa20e1380f5e9
Nathan Stitt <nathan@stitt.org>
2013-07-31 16:50:13 -0500

    Unit tests for remote_url

--- app/models/remote_url.rb
+++ app/models/remote_url.rb
@@ -2,7 +2,7 @@ require 'cgi'
 
 class RemoteUrl < ActiveRecord::Base
 
-  self.establish_connection( DC::ANALYTICS_DB )
+  self.establish_connection( DC::ANALYTICS_DB ) unless Rails.env.testing?
 
   DOCUMENT_CLOUD_URL = /^https?:\/\/(www\.)?documentcloud.org/
 
@@ -31,24 +31,24 @@ class RemoteUrl < ActiveRecord::Base
 
   def self.record_hits_on_document(doc_id, url, hits)
     url = url.mb_chars[0...255].to_s
-    row = self.find_or_create_by_document_id_and_url_and_date_recorded(doc_id, url, Time.now.utc.to_date)
+    row = self.find_or_create_by(:document_id=>doc_id, :url=>url, :date_recorded=>Time.now.utc.to_date)
     row.update_attributes :hits => row.hits + hits
-    
+
     # Increment the document's total hits.
     doc = Document.find_by_id(doc_id)
     doc.update_attributes(:hit_count => doc.hit_count + hits) if doc
   end
 
   def self.record_hits_on_search(query, url, hits)
-    url   = url.mb_chars[0...255].to_s
+    url   = url[0...255]
     query = CGI::unescape(query)
-    row   = self.find_or_create_by_search_query_and_url_and_date_recorded(query, url, Time.now.utc.to_date)
+    row   = self.find_or_create_by( :search_query=>query, :url=>url, :date_recorded=>Time.now.utc.to_date )
     row.update_attributes :hits => row.hits + hits
   end
 
   def self.record_hits_on_note(note_id, url, hits)
     url = url.mb_chars[0...255].to_s
-    row = self.find_or_create_by_note_id_and_url_and_date_recorded(note_id, url, Time.now.utc.to_date)
+    row = self.find_or_create_by( :note_id=>note_id, :url=>url, :date_recorded=>Time.now.utc.to_date )
     row.update_attributes :hits => row.hits + hits
   end
 
@@ -56,7 +56,7 @@ class RemoteUrl < ActiveRecord::Base
   # for all listed document ids. This method is only ever run within a
   # background job.
   def self.populate_detected_document_ids(doc_ids)
-    urls = self.aggregated.all(:conditions => {:document_id => doc_ids})
+    urls = self.aggregated.where({:document_id => doc_ids})
     top  = urls.inject({}) do |memo, url|
       if DOCUMENT_CLOUD_URL =~ url.url
         memo
@@ -66,19 +66,17 @@ class RemoteUrl < ActiveRecord::Base
         memo
       end
     end
-    Document.find_each(:conditions => {:id => top.keys}) do |doc|
-      doc.detected_remote_url = top[doc.id].url
-      doc.save if doc.changed?
+    Document.where(:id=>top.keys).find_in_batches do | documents |
+      documents.each do | doc |
+        doc.detected_remote_url = top[doc.id].url
+        doc.save if doc.changed?
+      end
     end
   end
 
-  def self.top_documents(days=7, options={})
-    hit_documents = self.by_document.all({
-      :conditions => ['date_recorded > ?', days.days.ago],
-      :having => ['sum(hits) > 0'],
-      :order => 'hits desc'
-    }.merge(options))
-    docs = Document.find_all_by_id(hit_documents.map {|u| u.document_id }).inject({}) do |memo, doc|
+  def self.top_documents( days=7, limit=1000 )
+    hit_documents = self.top_query( days, limit=1000 ).by_document
+    docs = Document.find( hit_documents.map(&:document_id) ).inject({}) do |memo, doc|
       memo[doc.id] = doc
       memo
     end
@@ -86,41 +84,29 @@ class RemoteUrl < ActiveRecord::Base
       url_attrs = url.attributes
       url_attrs[:url] = docs[url.document_id].published_url
       url_attrs[:id] = "#{url.document_id}:#{url_attrs[:url]}"
-      first_hit = RemoteUrl.first(:select => 'created_at',
-                                  :conditions => {:document_id => url['document_id']},
-                                  :order => 'created_at ASC')
-      url_attrs[:first_recorded_date] = first_hit[:created_at].strftime "%a %b %d, %Y"
+      first_hit = RemoteUrl.where( :document_id => url['document_id'] ).order('created_at ASC').pluck('created_at').first
+      url_attrs[:first_recorded_date] = first_hit.strftime "%a %b %d, %Y"
       docs[url.document_id].admin_attributes.merge(url_attrs)
     end
   end
-  
-  def self.top_searches(days=7, options={})
-    hit_searches = self.by_search_query.all({
-      :conditions => ['date_recorded > ?', days.days.ago],
-      :having => ['sum(hits) > 0'],
-      :order => 'hits desc'
-    }.merge(options))
+
+  def self.top_searches( days=7, limit=1000 )
+    hit_searches = self.top_query( days, limit ).by_search_query
     hit_searches.map do |query|
       query_attrs = query.attributes
-      first_hit = RemoteUrl.first(:select => 'created_at',
-                                  :conditions => {:search_query => query.search_query},
-                                  :order => 'created_at ASC')
-      query_attrs[:first_recorded_date] = first_hit[:created_at].strftime "%a %b %d, %Y"
+      first_hit = RemoteUrl.where( :search_query => query.search_query ).order('created_at ASC').pluck('created_at').first
+      query_attrs[:first_recorded_date] = first_hit.strftime "%a %b %d, %Y"
       query_attrs
     end
   end
-  
-  def self.top_notes(days=7, options={})
-    hit_notes = self.by_note.all({
-      :conditions => ['date_recorded > ?', days.days.ago],
-      :having => ['sum(hits) > 0'],
-      :order => 'hits desc'
-    }.merge(options))
-    notes = Annotation.find_all_by_id(hit_notes.map {|n| n.note_id }).inject({}) do |memo, note|
+
+  def self.top_notes(days=7, limit=1000 )
+    hit_notes = self.top_query( days, limit ).by_note
+    notes = Annotation.find( hit_notes.map(&:note_id) ).inject({}) do |memo, note|
       memo[note.id] = note.canonical.merge({:document_id => note.document_id})
       memo
     end
-    docs = Document.find_all_by_id(notes.map {|id, n| n[:document_id] }).inject({}) do |memo, doc|
+    docs = Document.find( notes.map {|id, n| n[:document_id] } ).inject({}) do |memo, doc|
       memo[doc.id] = doc
       memo
     end
@@ -128,12 +114,21 @@ class RemoteUrl < ActiveRecord::Base
       note_attrs = note.attributes
       note_attrs.delete :id
       note_attrs[:document] = docs[notes[note.note_id][:document_id]]
-      first_hit = RemoteUrl.first(:select => 'created_at',
-                                  :conditions => {:note_id => note.note_id},
-                                  :order => 'created_at ASC')
-      note_attrs[:first_recorded_date] = first_hit[:created_at].strftime "%a %b %d, %Y"
+      first_hit = RemoteUrl.where( {:note_id => note.note_id} ).order('created_at ASC').pluck('created_at').first
+      note_attrs[:first_recorded_date] = first_hit.strftime "%a %b %d, %Y"
       notes[note.note_id].merge(note_attrs)
     end
+
+  end
+
+  private
+
+  def self.top_query( days, limit )
+    self
+      .where(['date_recorded > ?', days.days.ago] )
+      .having( 'sum(hits) > 0' )
+      .order(  'sum(hits) desc' )
+      .limit(limit)
   end
 
 end
