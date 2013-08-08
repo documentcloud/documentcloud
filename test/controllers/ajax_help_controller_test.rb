85d0fefd1e49209b92b1e79aea9dcb722d5d10a6
Nathan Stitt <nathan@stitt.org>
2013-08-08 16:48:31 -0500

    Ajax Controller functional test

---
+++ test/controllers/ajax_help_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class AjaxHelpControllerTest < ActionController::TestCase
+
+
+  it "sends contact emails" do
+    login_account!
+    assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+      get :contact_us, :message=>'Wake Up!'
+    end
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['support@documentcloud.org'], mail.to
+    assert_equal [louis.email], mail.reply_to
+    assert_match( /Wake Up!/, mail.body.to_s )
+  end
+
+end
