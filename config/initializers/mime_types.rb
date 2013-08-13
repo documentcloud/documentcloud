e77bb56292af1e07d8158690ff55137f244a59fb
Nathan Stitt <nathan@stitt.org>
2013-08-13 16:25:12 -0500

    RDF is used by documents_controller#show

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,9 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register_alias "text/html", :iphone
+Mime::Type.register "application/rdf+xml", :rdf
 
+#Mime::Type.register_alias "text/html", :iphone
+#Mime::Type.register "text/richtext", :rtf
 #Mime::Type.register 'application/pdf',      :pdf
 #Mime::Type.register 'application/zip',      :zip
 #Mime::Type.register 'application/rdf+xml',  :rdf
