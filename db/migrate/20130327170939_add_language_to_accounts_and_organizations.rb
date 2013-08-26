dccb8bdabac1a76f0d95abda6d5fc544c853ec04
Ted Han <ted@knowtheory.net>
2013-08-25 22:10:51 -0500

    Add language defaults to Accounts and Organizations.

--- db/migrate/20130327170939_add_language_to_accounts_and_organizations.rb
+++ db/migrate/20130327170939_add_language_to_accounts_and_organizations.rb
@@ -1,7 +1,10 @@
 class AddLanguageToAccountsAndOrganizations < ActiveRecord::Migration
   def self.up
-    add_column :accounts, :language,  :string, :limit => 3
-    add_column :organizations, :language,  :string, :limit => 3
+    add_column :accounts,      :language,  :string, :limit => 3, :default => "eng"
+    add_column :organizations, :language,  :string, :limit => 3, :default => "eng"
+    
+    Account.update_all :language=>"eng"
+    Organization.update_all :language=>"eng"
   end
 
   def self.down
