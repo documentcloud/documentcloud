107ad0d17de22707ab3cf63dc7f5a65258af44c8
Ted Han <ted@knowtheory.net>
2013-10-11 04:51:15 -0500

    Hide/simplify things.

--- public/javascripts/ui/accounts/organization_manager.js
+++ public/javascripts/ui/accounts/organization_manager.js
@@ -2,7 +2,7 @@ dc.ui.OrganizationManager = Backbone.View.extend({
   className: 'organization',
   initialize: function(options){
     this.membership = options.membership;
-    this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'row'}); });
+    //this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'organization_row'}); });
   },
   render: function() {
     this.$el.html(JST['organization/details']({
@@ -10,12 +10,11 @@ dc.ui.OrganizationManager = Backbone.View.extend({
       membership:   this.describeMembership(this.membership),
       languages: dc.language.NAMES
     }));
-    this.$('.account_list_content').append(this.memberViews.map(function(view){ return view.render().el; }));
+    //this.$('.account_list_content').append(this.memberViews.map(function(view){ return view.render().el; }));
     this.$el.addClass('account_list');
     return this;
   },
   describeMembership: function(membership){
-    console.log(this.membership.get('role'));
     return "a " + dc.model.Account.prototype.ROLE_NAMES[membership.get('role')] + " for ";
   }
 });
