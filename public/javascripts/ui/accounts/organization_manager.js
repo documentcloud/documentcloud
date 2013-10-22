3040467f72133dc83d0e7a8a7da541f5f58e1373
Ted Han <ted@knowtheory.net>
2013-10-21 23:24:09 -0500

    Add list of accounts back into organization displays

--- public/javascripts/ui/accounts/organization_manager.js
+++ public/javascripts/ui/accounts/organization_manager.js
@@ -2,7 +2,7 @@ dc.ui.OrganizationManager = Backbone.View.extend({
   className: 'organization',
   initialize: function(options){
     this.membership = options.membership;
-    //this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'organization_row'}); });
+    this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'row', dialog: options.dialog}); }, this);
   },
   render: function() {
     this.$el.html(JST['organization/details']({
@@ -10,7 +10,7 @@ dc.ui.OrganizationManager = Backbone.View.extend({
       membership:   this.describeMembership(this.membership),
       languages: dc.language.NAMES
     }));
-    //this.$('.account_list_content').append(this.memberViews.map(function(view){ return view.render().el; }));
+    this.$('.account_list_content').append(this.memberViews.map(function(view){ return view.render().el; }));
     this.$el.addClass('account_list');
     return this;
   },
