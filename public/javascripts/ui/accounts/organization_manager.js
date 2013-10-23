aa2570e709fd0576b601fdfc5b3279f7a0255f74
Ted Han <ted@knowtheory.net>
2013-10-23 00:17:46 -0500

    Showing off the table view again.

--- public/javascripts/ui/accounts/organization_manager.js
+++ public/javascripts/ui/accounts/organization_manager.js
@@ -2,7 +2,7 @@ dc.ui.OrganizationManager = Backbone.View.extend({
   className: 'organization',
   initialize: function(options){
     this.membership = options.membership;
-    this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'organization_row', dialog: options.dialog}); }, this);
+    this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'row', dialog: options.dialog}); }, this);
   },
   render: function() {
     this.$el.html(JST['organization/details']({
