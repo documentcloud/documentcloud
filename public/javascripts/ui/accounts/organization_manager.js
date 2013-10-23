c569d423736c72d17eadfb7af98cef8ad47e7e6d
Ted Han <ted@knowtheory.net>
2013-10-23 05:52:00 -0500

    New account button for organization pane.

--- public/javascripts/ui/accounts/organization_manager.js
+++ public/javascripts/ui/accounts/organization_manager.js
@@ -1,20 +1,44 @@
 dc.ui.OrganizationManager = Backbone.View.extend({
   className: 'organization',
-  initialize: function(options){
-    this.membership = options.membership;
-    this.memberViews = this.model.members.map(function(account){ return new dc.ui.AccountView({model: account, kind: 'row', dialog: options.dialog}); }, this);
+
+  events: {
+    'click .save_changes': 'saveOrganization',
+    'click .new_account' : 'newAccount'
   },
+
+  initialize: function(options){
+    this.dialog = options.dialog;
+    this.membership = options.membership;
+    this.memberViews = this.model.members.map(function(account){ 
+      return new dc.ui.AccountView({model: account, kind: 'row', dialog: this.dialog}); 
+    }, this);
+  },
+
   render: function() {
     this.$el.html(JST['organization/details']({
       organization: this.model, 
       membership:   this.describeMembership(this.membership),
       languages: dc.language.NAMES
     }));
-    this.$('.account_list_content').append(this.memberViews.map(function(view){ return view.render().el; }));
+    this.list = this.$('.account_list_content');
+    this.list.append(this.memberViews.map(function(view){ return view.render().el; }));
     this.$el.addClass('account_list');
     return this;
   },
+
   describeMembership: function(membership){
     return "a " + dc.model.Account.prototype.ROLE_NAMES[membership.get('role')] + " for ";
-  }
+  },
+
+  newAccount : function() {
+    console.log("new account");
+    var view = new dc.ui.AccountView({
+      model : new dc.model.Account(),
+      kind : 'row',
+      dialog : this.dialog
+    });
+    this.list.prepend(view.render('edit').el);
+  },
+  
+  saveOrganization: function() { console.log('saving') }
 });
