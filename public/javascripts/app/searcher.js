c91a8a641dca0217012fe21865e4ae7eb018702e
Nathan Stitt <nathan@stitt.org>
2013-10-24 15:37:55 -0500

    Prevent searching for "null" when term is missing

--- public/javascripts/app/searcher.js
+++ public/javascripts/app/searcher.js
@@ -211,7 +211,7 @@ dc.controllers.Searcher = Backbone.Router.extend({
   // When searching by the URL's hash value, we need to unescape first.
   searchByHash : function(query, page) {
     _.defer(_.bind(function() {
-      this.search(decodeURIComponent(query), page && parseInt(page, 10));
+      this.search(query ? decodeURIComponent(query) : '', page && parseInt(page, 10));
     }, this));
   },
 
