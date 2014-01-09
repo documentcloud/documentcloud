5dcdec14cee14f8d6fba3dd2837fd3d106f1afe9
Nathan Stitt <nathan@stitt.org>
2014-01-09 15:14:52 -0600

    Silence annoying i18n locales deprecation message

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module DC
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.i18n.enforce_available_locales = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
