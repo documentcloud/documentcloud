12f0e91d9f8355515c254f27efffcf7a0e50c3c5
Nathan Stitt <nathan@stitt.org>
2014-02-14 16:57:12 -0600

    Pull in updated work from translators

--- lib/dc/language.rb
+++ lib/dc/language.rb
@@ -3,17 +3,26 @@ module DC
 
   # should we be using iso15924 script codes?
   #   http://www.unicode.org/iso15924/iso15924-codes.html
+
+  # Needs to be kept in sync with JS version in
+  # lib/dc/language.js
   module Language
     NAMES = {
       'eng' => 'English',
       'spa' => 'Spanish',
       'fra' => 'French',
-      'deu' => 'German'
+      'deu' => 'German',
+      'dan'=> 'Danish',
+      'chi'=> 'Chinese (Traditional)',
+      'rus'=> 'Russian',
+      'chi'=> 'Chinese',
+      'ukr'=> 'Ukrainian'
+
     }
     SUPPORTED = NAMES.keys
     DEFAULT = 'eng'
-
-    USER = ['spa','eng']
+    
+    USER = ['spa','eng','chi','rus','dan','chi','ukr']
   end
 
 end
