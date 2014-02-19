8bd65168808b6f576d669d4ffcf188ab332b7c55
Nathan Stitt <nathan@stitt.org>
2014-02-19 16:00:07 -0600

    Fix slug generation for long titles without spaces
    If a Document title had a long title (>50 characters)
    without any spaces, the #ensure_titled method would fail
    to extract the words and would set the slug to blank.
    The document would then fail the "Slug can't be blank" validation
    and fail to save.
    
    The change attempts to first split on spaces, underscores, and dashes.
    If that fails, it will just take the first 50 characters of the title.

--- app/models/document.rb
+++ app/models/document.rb
@@ -933,8 +933,8 @@ class Document < ActiveRecord::Base
     slugged.downcase!         # Ensure lowercase.
     # Truncate to the nearest space.
     if slugged.length > 50
-      words = slugged[0...50].split(' ')
-      slugged = words[0, words.length - 1].join(' ')
+      words = slugged[0...50].split(/\s|_|-/)
+      slugged = words.length > 1 ? words[0, words.length - 1].join(' ') : words.first
     end
     slugged.gsub!(' ', '-')   # Dasherize spaces.
     self.slug = slugged
