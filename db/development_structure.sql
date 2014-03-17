3e42567564db2315e707b78fa998c1627ecebbf7
Nathan Stitt <nathan@stitt.org>
2014-03-17 17:01:29 -0500

    Set default language to be 'eng'
    This is a complete hack - since the production db
    has the language columns defaulting to 'eng', we
    have to match it on development.  Otherwise new boxes get
    setup wrong when they load the structure to seed their db.

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -27,7 +27,8 @@ CREATE TABLE accounts (
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
     identities hstore,
-    language character varying(3)
+    language character varying(3) default 'eng',
+    document_language character varying(3) default 'eng'
 );
 
 
@@ -449,7 +450,8 @@ CREATE TABLE organizations (
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
     demo boolean DEFAULT false NOT NULL,
-    language character varying(3)
+    language character varying(3) default 'eng',
+    document_language character varying(3) default 'eng'
 );
 
 
