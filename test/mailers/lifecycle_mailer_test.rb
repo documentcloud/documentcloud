1768b69d16e4d6ceb7dfcc5ee7bb3d40648e7e0f
Nathan Stitt <nathan@stitt.org>
2014-03-26 16:55:42 -0500

    Test that reviewer email includes document name

--- test/mailers/lifecycle_mailer_test.rb
+++ test/mailers/lifecycle_mailer_test.rb
@@ -22,7 +22,7 @@ class LifecycleMailerTest < ActionMailer::TestCase
     assert_equal [ accounts(:admin).email ], email.cc
     assert_equal [ joe.email ], email.to
     assert_equal 'Review "Secret Plans" on DocumentCloud', email.subject
-    assert_equal read_fixture('reviewer_instructions'), email.body.to_s
+    assert_match( /Admin startor at The Daily Tribune has invited you to review "Secret Plans."/, email.body.to_s )
   end
 
   def test_reset_request
