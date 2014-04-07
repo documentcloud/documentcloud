ddb52c92e7836735c95d30887760e40a78e48748
Nathan Stitt <nathan@stitt.org>
2014-04-07 11:30:20 -0500

    Turn off json's root encoding.
    ActiveModel serializers are brought in via CloudCrowd.
    
    If they are present, 4.1 uses them for serializing JSON
    when a collection of ActiveRecord models is rendered
    via ``render :json=>collection``, as the
    ``application_controller#json`` method does.

---
+++ config/initializers/rails_defaults.rb
@@ -0,0 +1,5 @@
+# Disable for all serializers (except ArraySerializer)
+ActiveModel::Serializer.root = false
+
+# Disable for ArraySerializer
+ActiveModel::ArraySerializer.root = false
--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -8,7 +8,3 @@ ActiveSupport.on_load(:action_controller) do
   wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
 end
 
-# To enable root element in JSON for ActiveRecord objects.
-ActiveSupport.on_load(:active_record) do
- self.include_root_in_json = false
-end
