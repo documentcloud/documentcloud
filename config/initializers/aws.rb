7b2d8873d2745b3583437be63579b37e3922a161
Ted Han <ted@knowtheory.net>
2014-04-09 09:25:07 -0500

    Drop RightHttpConnection, and move to full Rails4.1

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,5 @@
 # More generous HTTP timeouts ... mostly because Solr is unresponsive.
 
-Rightscale::HttpConnection.params[:http_connection_open_timeout] = 30
-Rightscale::HttpConnection.params[:http_connection_read_timeout] = 300
-
 AWS.config(
   :access_key_id     => DC::SECRETS['aws_access_key'],
   :secret_access_key => DC::SECRETS['aws_secret_key'],
