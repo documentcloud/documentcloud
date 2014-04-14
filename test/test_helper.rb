5524ef6b29749d1935f90f7cc13ec27b8216af2e
Nathan Stitt <nathan@stitt.org>
2014-04-14 15:37:27 -0500

    Remove turn configuration from tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,25 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'rails/test_help'
 require 'sunspot_matchers/test_helper'
-require 'turn/autorun'
 
 PROCESSING_JOBS = []
-
-Turn.config do |c|
-  # use one of output formats:
-  # :outline  - turn's original case/test outline mode [default]
-  # :progress - indicates progress with progress bar
-  # :dotted   - test/unit's traditional dot-progress mode
-  # :pretty   - new pretty reporter
-  # :marshal  - dump output as YAML (normal run mode only)
-  # :cue      - interactive testing
-  c.format  = :pretty
-  # turn on invoke/execute tracing, enable full backtrace
-  c.trace   = 6
-  # use humanized test names (works only with :outline format)
-  c.natural = true
-end
-
 
 module DocumentCloudAssertions
   def self.included(base)
