67702c248bf1a366eb66df2eea416500c11c1984
Ted Han <ted@knowtheory.net>
2014-04-19 07:27:53 -0500

    Hack CloudCrowd models into the rails console.

---
+++ config/initializers/cloud_crowd.rb
@@ -0,0 +1,13 @@
+ENV['RAILS_ENV'] ||= (Rails.env || 'development')
+
+CloudCrowd.configure("./config/cloud_crowd/#{Rails.env}/config.yml")
+
+[CloudCrowd::Job, CloudCrowd::WorkUnit, CloudCrowd::NodeRecord].each do |klass|
+  klass.class_eval do
+    config_path = "./config/cloud_crowd/#{Rails.env}/database.yml"
+    configuration = YAML.load(ERB.new(File.read(config_path)).result)
+    self.establish_connection(configuration)
+  end
+end
+
+include CloudCrowd
