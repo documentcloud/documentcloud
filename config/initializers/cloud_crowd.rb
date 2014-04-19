45e8e03169609e26ec03c18d1687be69fb3c419a
Ted Han <ted@knowtheory.net>
2014-04-19 07:32:28 -0500

    How about we only load cloud crowd in the console.

--- config/initializers/cloud_crowd.rb
+++ config/initializers/cloud_crowd.rb
@@ -1,13 +1,17 @@
-ENV['RAILS_ENV'] ||= (Rails.env || 'development')
+# Running in the console? 
+# Here's direct access to the CloudCrowd database.
+if defined? Rails::Console
+  ENV['RAILS_ENV'] ||= (Rails.env || 'development')
 
-CloudCrowd.configure("./config/cloud_crowd/#{Rails.env}/config.yml")
+  CloudCrowd.configure("./config/cloud_crowd/#{Rails.env}/config.yml")
 
-[CloudCrowd::Job, CloudCrowd::WorkUnit, CloudCrowd::NodeRecord].each do |klass|
-  klass.class_eval do
-    config_path = "./config/cloud_crowd/#{Rails.env}/database.yml"
-    configuration = YAML.load(ERB.new(File.read(config_path)).result)
-    self.establish_connection(configuration)
+  [CloudCrowd::Job, CloudCrowd::WorkUnit, CloudCrowd::NodeRecord].each do |klass|
+    klass.class_eval do
+      config_path = "./config/cloud_crowd/#{Rails.env}/database.yml"
+      configuration = YAML.load(ERB.new(File.read(config_path)).result)
+      self.establish_connection(configuration)
+    end
   end
-end
 
-include CloudCrowd
+  include CloudCrowd
+end
