fba9195c8021d6afadc5fe47032dbba08a6aeec9
Ted Han <ted@knowtheory.net>
2014-06-16 15:36:51 -0500

    Remove dynamic finder.

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -21,19 +21,18 @@ class ImportController < ApplicationController
   end
 
   # Returning a "201 Created" ack tells CloudCrowd to clean up the job.
-  # If the document failed to import, we remove it.
   def cloud_crowd
     job = JSON.parse(params[:job])
     if job['status'] != 'succeeded'
       logger.error("Document import failed: " + job.inspect)
-      record = ProcessingJob.find_by_cloud_crowd_id(job['id'])
+      record = ProcessingJob.where(:cloud_crowd_id=>job['id']).last
       record.document.update_attributes(:access => DC::Access::ERROR) if record && record.document
     end
     ProcessingJob.destroy_all(:cloud_crowd_id => job['id'])
     #render :text => '201 Created', :status => 201
     render :text => "Created but don't clean up the job right now."
   end
-
+  
   # CloudCrowd is done changing the document's asset access levels.
   # 201 created cleans up the job.
   def update_access
