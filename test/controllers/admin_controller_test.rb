e5076b2771aa730344e9167834cda9b712ed5899
Nathan Stitt <nathan@stitt.org>
2014-11-11 09:01:12 -0600

    Offer to link existing account to new organization
    Instead of refusing to create a new organization when the email is already in
    use, add an additional "relink" checkbox control.  If the checkbox is selected,
    add an additional membership for the account and set it as the default.

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -19,4 +19,43 @@ class AdminControllerTest < ActionController::TestCase
     assert Account.where( "email"=>"foo+nathan@documentcloud.org" ).first
   end
 
+  def test_it_checks_for_existing_accounts
+    login_account! :admin
+    params = {
+      "organization"=>{"name"=>"nathan",
+                       "slug"=>"testing",
+                       "language"=>"eng",
+                       "document_language"=>"eng"},
+      "account"=>{"email"=>louis.email,
+                  "first_name"=>"test",
+                  "last_name"=>"bob"}}
+    post :signup, params
+    assert_match louis.email, flash[:error]
+    assert_nil Organization.where( :slug=>'testing' ).first
+  end
+
+  def test_it_moves_accounts
+    login_account! :admin
+    params = {
+      "move_account"=>"t",
+      "organization"=>{"name"=>"nathan",
+                       "slug"=>"testing",
+                       "language"=>"eng",
+                       "document_language"=>"eng"},
+      "account"=>{"email"=>louis.email,
+                  "first_name"=>"test",
+                  "last_name"=>"bob"}}
+    assert_difference ->{ Organization.count }, 1 do
+      post :signup, params
+    end
+    assert_nil flash[:error]
+    org = Organization.find_by_slug('testing')
+    louis.memberships(true)
+
+    assert_equal 2, louis.memberships.count
+    assert louis.memberships.where(organization: tribune, default:false).any?
+    assert louis.memberships.where(organization: org, default:true).any?
+  end
+
+
 end
