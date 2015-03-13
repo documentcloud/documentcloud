049ccf803d3fbf3f60276278c4acc66f9c9d8290
Ted Han <ted@knowtheory.net>
2015-03-13 18:02:41 -0400

    Consolidate page cache expiry & always clear cache on update.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -50,10 +50,8 @@ class DocumentsController < ApplicationController
                          :related_article, :remote_url, :publish_at, :data, :language)
     success = doc.secure_update attrs, current_account
     return json(doc, 403) unless success
-    if doc.cacheable?
-      expire_page doc.canonical_cache_path
-      doc.annotations.each{ |note| expire_page note.canonical_cache_path }
-    end
+
+    clear_current_document_cache
     Document.populate_annotation_counts(current_account, [doc])
     json doc
   end
@@ -64,10 +62,8 @@ class DocumentsController < ApplicationController
       doc.errors.add(:base, "You don't have permission to delete the document." )
       return json(doc, 403)
     end
-    if doc.cacheable?
-      expire_page doc.canonical_cache_path
-      doc.annotations.each{ |note| expire_page note.canonical_cache_path }
-    end
+
+    clear_current_document_cache
     doc.destroy
     json nil
   end
@@ -277,5 +273,9 @@ class DocumentsController < ApplicationController
     return false unless current_document(true)
     @current_page ||= current_document.pages.find_by_page_number(num.to_i)
   end
-
+  
+  def clear_current_document_cache
+    expire_page current_document.canonical_cache_path
+    current_document.annotations.each{ |note| expire_page note.canonical_cache_path }
+  end
 end
--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -24,10 +24,16 @@ class ImportController < ApplicationController
   # If the document failed to import, we remove it.
   def cloud_crowd
     job = JSON.parse(params[:job])
-    if job['status'] != 'succeeded'
-      logger.error("Document import failed: " + job.inspect)
-      record = ProcessingJob.find_by_cloud_crowd_id(job['id'])
-      record.document.update_attributes(:access => DC::Access::ERROR) if record && record.document
+    record = ProcessingJob.find_by_cloud_crowd_id(job['id'])
+
+    if record and record.document
+      document = record.document
+
+      if job['status'] != 'succeeded'
+        logger.error("Document import failed: " + job.inspect)
+        document.update_attributes(:access => DC::Access::ERROR)
+      end
+      expire_document_cache
     end
     ProcessingJob.destroy_all(:cloud_crowd_id => job['id'])
     #render :plain => '201 Created', :status => 201
@@ -37,7 +43,17 @@ class ImportController < ApplicationController
   # CloudCrowd is done changing the document's asset access levels.
   # 201 created cleans up the job.
   def update_access
+    expire_document_cache
     render :plain => '201 Created', :status => 201
   end
 
+  def expire_document_cache
+    job = JSON.parse(params[:job])
+    record = ProcessingJob.find_by_cloud_crowd_id(job['id'])
+
+    if record and record.document and record.document.cacheable?
+      expire_page record.document.canonical_cache_path
+      record.document.annotations.each{ |note| expire_page note.canonical_cache_path }
+    end
+  end
 end
