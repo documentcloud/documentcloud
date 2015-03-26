9898c6f5f3bb4f5e3a9cee7d7f699246596d13e0
Justin Reese <justin@justinreese.com>
2015-03-26 17:20:23 -0500

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 1) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "accounts", force: true do |t|
     t.string   "first_name",        limit: 40
@@ -33,17 +32,16 @@ ActiveRecord::Schema.define(version: 1) do
   add_index "accounts", ["identities"], name: "index_accounts_on_identites", using: :gin
 
   create_table "annotations", force: true do |t|
-    t.integer  "organization_id",                null: false
-    t.integer  "account_id",                     null: false
-    t.integer  "document_id",                    null: false
-    t.integer  "page_number",                    null: false
-    t.integer  "access",                         null: false
-    t.text     "title",                          null: false
+    t.integer  "organization_id",            null: false
+    t.integer  "account_id",                 null: false
+    t.integer  "document_id",                null: false
+    t.integer  "page_number",                null: false
+    t.integer  "access",                     null: false
+    t.text     "title",                      null: false
     t.text     "content"
-    t.string   "location",            limit: 40
+    t.string   "location",        limit: 40
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "moderation_approval"
   end
 
   add_index "annotations", ["document_id"], name: "index_annotations_on_document_id", using: :btree
@@ -51,13 +49,6 @@ ActiveRecord::Schema.define(version: 1) do
   create_table "app_constants", force: true do |t|
     t.string "key"
     t.string "value"
-  end
-
-  create_table "cloud_crowd_stats", force: true do |t|
-    t.tsrange "period"
-    t.integer "pending_count"
-    t.integer "average_wait"
-    t.integer "processing_count"
   end
 
   create_table "collaborations", force: true do |t|
@@ -101,8 +92,8 @@ ActiveRecord::Schema.define(version: 1) do
     t.boolean  "text_changed",                     default: false, null: false
     t.integer  "hit_count",                        default: 0,     null: false
     t.integer  "public_note_count",                default: 0,     null: false
-    t.integer  "reviewer_count",                   default: 0,     null: false
     t.integer  "file_size",                        default: 0,     null: false
+    t.integer  "reviewer_count",                   default: 0,     null: false
     t.integer  "char_count",                       default: 0,     null: false
     t.string   "original_extension"
     t.text     "file_hash"
@@ -112,7 +103,6 @@ ActiveRecord::Schema.define(version: 1) do
   add_index "documents", ["account_id"], name: "index_documents_on_account_id", using: :btree
   add_index "documents", ["file_hash"], name: "index_documents_on_file_hash", using: :btree
   add_index "documents", ["hit_count"], name: "index_documents_on_hit_count", using: :btree
-  add_index "documents", ["organization_id"], name: "foo2", using: :btree
   add_index "documents", ["public_note_count"], name: "index_documents_on_public_note_count", using: :btree
 
   create_table "entities", force: true do |t|
@@ -147,7 +137,7 @@ ActiveRecord::Schema.define(version: 1) do
     t.string   "organization",              null: false
     t.date     "article_date",              null: false
     t.text     "writeup",                   null: false
-    t.integer  "present_order", default: 0
+    t.integer  "present_order", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -169,8 +159,8 @@ ActiveRecord::Schema.define(version: 1) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "demo",                          default: false, null: false
-    t.string   "language",          limit: 3
-    t.string   "document_language", limit: 3
+    t.string   "language",          limit: 3,   default: "eng"
+    t.string   "document_language", limit: 3,   default: "eng"
   end
 
   add_index "organizations", ["name"], name: "index_organizations_on_name", unique: true, using: :btree
@@ -190,22 +180,6 @@ ActiveRecord::Schema.define(version: 1) do
   add_index "pages", ["document_id"], name: "index_pages_on_document_id", using: :btree
   add_index "pages", ["page_number"], name: "index_pages_on_page_number", using: :btree
   add_index "pages", ["start_offset", "end_offset"], name: "index_pages_on_start_offset_and_end_offset", using: :btree
-
-  create_table "pending_memberships", force: true do |t|
-    t.string   "first_name",                        null: false
-    t.string   "last_name",                         null: false
-    t.string   "email",                             null: false
-    t.string   "organization_name",                 null: false
-    t.string   "usage",                             null: false
-    t.string   "editor"
-    t.string   "website"
-    t.boolean  "validated",         default: false, null: false
-    t.text     "notes"
-    t.integer  "organization_id"
-    t.hstore   "fields"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "processing_jobs", force: true do |t|
     t.integer "account_id",     null: false
@@ -243,9 +217,9 @@ ActiveRecord::Schema.define(version: 1) do
     t.integer "organization_id", null: false
     t.integer "account_id",      null: false
     t.integer "document_id",     null: false
+    t.integer "access",          null: false
     t.text    "title",           null: false
     t.integer "page_number",     null: false
-    t.integer "access",          null: false
   end
 
   add_index "sections", ["document_id"], name: "index_sections_on_document_id", using: :btree
