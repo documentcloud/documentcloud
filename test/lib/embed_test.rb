da67f74fbb8041ebfed74e4880c474fa8846ffc5
Ted Han <ted@knowtheory.net>
2015-04-14 15:13:45 -0500

    more stub tests, add fake url.

--- test/lib/embed_test.rb
+++ test/lib/embed_test.rb
@@ -1,11 +1,18 @@
 require_relative '../test_helper'
+require 'pry'
 
 describe DC::Embed do
+  
+  let(:resource) { Struct.new(:id, :url).new("1235", "https://lol.wat/1235") }
+  
   it "should require a resource" do
     Proc.new{ DC::Embed.new(nil, {}) }.must_raise ArgumentError
     Proc.new{ DC::Embed.new({}, {})  }.must_raise ArgumentError
 
-    struct = Struct.new(:id).new
-    DC::Embed.new(struct, {}).must_be_kind_of DC::Embed
+    DC::Embed.new(resource, {}).must_be_kind_of DC::Embed
   end
+  
+  it "should output HTML markup"
+  it "should output an oembed response as json"
+  it "should output a JST template"
 end
