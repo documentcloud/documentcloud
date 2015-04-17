f6c321cae9606c6643a77a0affd479d9a5fb9382
Justin Reese <justin@justinreese.com>
2015-04-17 14:46:20 -0500

    Terminate function declaration w/semicolons for WP
    WordPress strips newlines, which means we have to be extra-alert for function declarations like `var foo = function(){…}` that don't end in a semicolon.

--- lib/dc/embed.rb
+++ lib/dc/embed.rb
@@ -259,18 +259,18 @@ module DC
             var pendingQueue = window.DV._documentsWaitingForAppLoad = [];
             window.DV.load = function(resource, options) {
               pendingQueue.push({url: resource, options: options});
-            }
+            };
             
             var eventuallyLoadDocuments = function(){
               if (window.DV.viewers) {
-                for (i=0; i<pendingQueue.length; i++){ 
+                for (var i=0; i<pendingQueue.length; i++){ 
                   resource = pendingQueue[i];
                   DV.load(resource.url, resource.options);
                 }
               } else {
                 setTimeout(eventuallyLoadDocuments, 500);
               }
-            }
+            };
             eventuallyLoadDocuments();
 
             var loadCSS = function(url, media) {
