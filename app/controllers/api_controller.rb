e07738915dea936429ceaadc91f33972489fbdfd
Justin Reese <justin@justinreese.com>
2015-04-20 16:49:23 -0500

    Serialize, don't seralize, the mock resource

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -179,13 +179,13 @@ class ApiController < ApplicationController
     resource_params = Rails.application.routes.recognize_path(url.path) rescue nil
     return not_found unless url.host == DC::CONFIG['server_root'] and resource_embeddable?(resource_params)
 
-    # create a seralizer mock/class/struct for temporary use
-    resource_seralizer_klass = Struct.new(:id, :url, :js_url, :type)
+    # create a serializer mock/class/struct for temporary use
+    resource_serializer_klass = Struct.new(:id, :url, :js_url, :type)
     # The JS URL should match the protocol used for the request, but
     # the "canonical" resource URL can be our preferred one (HTTPS).
     resource_url    = url_for(resource_params.merge(:format => 'html', :protocol => 'https'))
     resource_js_url = url_for(resource_params.merge(:format => 'js'))
-    resource = resource_seralizer_klass.new(resource_params[:id], resource_url, resource_js_url, :document)
+    resource = resource_serializer_klass.new(resource_params[:id], resource_url, resource_js_url, :document)
     
     config = pick(params, *DC::Embed.embed_klass(resource.type).config_keys)
     embed = DC::Embed.embed_for(resource, config, {:strategy => :oembed})
