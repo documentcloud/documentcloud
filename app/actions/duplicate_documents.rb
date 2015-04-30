3baa2028c98a09d502cc1c3b1c879fa7de55e39d
Ted Han <ted@knowtheory.net>
2015-04-30 15:20:15 -0500

    Update DuplicateDocuments to not halt on any failure & to email a manifest upon completion.

--- app/actions/duplicate_documents.rb
+++ app/actions/duplicate_documents.rb
@@ -3,21 +3,27 @@ require File.dirname(__FILE__) + '/support/setup'
 class DuplicateDocuments < CloudCrowd::Action
 
   def process
-      Document.where( {:id => input} ).find_each do |doc|
-        begin
-          doc.duplicate!(account, options)
-        rescue => e
-          LifecycleMailer.exception_notification(e, options.merge({:document_id=>doc.id}) ).deliver
-          raise e
-        end
+    outcomes = {:succeeded=>[], :failed => []}
+    Document.where( {:id => input} ).find_each do |doc|
+      begin
+        doc.duplicate!(account, options)
+      rescue => e
+        LifecycleMailer.exception_notification(e, options.merge({:document_id=>doc.id}) ).deliver
+        outcomes[:failed].push({:id=>doc.id, :access=>doc.access, :error=>{:name=>e.class.name, :message=>e.message})
       end
-      true
+      outcomes[:succeeded].push(doc.id)
+    end
+    outcomes
   end
-
+  
+  def merge
+    # email manifest to us.
+    LifecycleMailer.logging_email("DuplicateDocument batch manifest", inputs)
+  end
+  
   private
 
   def account
     @account ||= Account.find(options['account_id']) if options['account_id']
   end
-
 end
