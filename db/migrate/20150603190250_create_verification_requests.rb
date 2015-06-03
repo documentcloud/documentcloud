2ae225694d01d9fb5a69531074daab79e4aefd85
Ted Han <ted@knowtheory.net>
2015-06-03 15:46:12 -0400

    formatting

--- db/migrate/20150603190250_create_verification_requests.rb
+++ db/migrate/20150603190250_create_verification_requests.rb
@@ -1,27 +1,27 @@
 class CreateVerificationRequests < ActiveRecord::Migration
   def change
     create_table :verification_requests do |t|
-
+      
       t.timestamps
-      t.string  "requester_email",      :null   => false
-      t.string  "requester_first_name", :null   => false
-      t.string  "requester_last_name",  :null   => false
+      t.string  "requester_email",                         :null => false
+      t.string  "requester_first_name",                    :null => false
+      t.string  "requester_last_name",                     :null => false
       t.string  "requester_notes",      :length => 255
-      t.string  "organization_name",    :length => 255, :null => false
-      t.string  "organization_slug",    :length => 255, :null => false
-      t.string  "organization_url",     :length => 255
-      t.string  "approver_email",       :length => 255, :null => false
-      t.string  "approver_first_name",  :null => false
-      t.string  "approver_last_name",   :null => false
-      t.string  "country"
-      t.string  "display_language",     :null => false
-      t.string  "document_language",    :null => false
+      t.string  "organization_name",    :length => 255,    :null => false
+      t.string  "organization_slug",    :length => 255,    :null => false
+      t.string  "organization_url",     :length => 255     
+      t.string  "approver_email",       :length => 255,    :null => false
+      t.string  "approver_first_name",                     :null => false
+      t.string  "approver_last_name",                      :null => false
+      t.string  "country"                                  
+      t.string  "display_language",                        :null => false
+      t.string  "document_language",                       :null => false
       t.string  "verification_notes"
-      t.integer "status",               :default => 1, :null => false
+      t.integer "status",               :default => 1,     :null => false
       t.boolean "agreed_to_terms",      :default => false, :null => false
       t.boolean "authorized_posting",   :default => false, :null => false
-      t.string  "signup_key",           :null => false
-      t.integer "account_id"
+      t.string  "signup_key",                              :null => false
+      t.integer "account_id" 
     end
   end
 end
