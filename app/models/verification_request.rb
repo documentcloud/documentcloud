e81830203c2f4f35831de088c18531dc0b14e6f7
Ted Han <ted@knowtheory.net>
2015-06-03 16:16:23 -0400

    Some validations.

--- app/models/verification_request.rb
+++ app/models/verification_request.rb
@@ -1,2 +1,30 @@
 class VerificationRequest < ActiveRecord::Base
+  REJECTED = 0
+  PENDING  = 1
+  VERIFIED = 2
+  SEE_NOTE = 3
+  
+  STATUS_MAP = {
+    rejected: 0,
+    pending:  1,
+    verified: 2,
+    see_note: 3
+  }
+  STATUS_NAMES = STATUS_MAP.invert
+  
+  belongs_to :account
+  
+  validates :requester_email, :approver_email
+    presence: true, 
+    format: { :with => DC::Validators::EMAIL }
+  
+  validates_presence :requester_first_name, :requester_last_name, :approver_first_name, :approver_last_name,
+                     :organization_name,
+    presence: true
+  
+  validates :display_language, :inclusion=>{ :in => DC::Language::USER,
+    :message => "must be one of: (#{DC::Language::USER.join(', ')})" }
+  validates :document_language,  :inclusion=>{ :in => DC::Language::SUPPORTED,
+    :message => "must be one of: (#{DC::Language::SUPPORTED.join(', ')})" }
+    
 end
