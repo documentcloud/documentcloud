1385939149206ea4a63fa49e252ad2ae837d0bd4
Justin Reese <justin@justinreese.com>
2015-06-11 18:23:45 -0500

    Progress checkin of signup page and v2 forms
    Incomplete, not functional, and lots of rough edges. Much style consolidation needed.

--- public/javascripts/app/signup.js
+++ public/javascripts/app/signup.js
@@ -3,6 +3,36 @@
 
 $(function() {
 
+  // This is a terrible function that only exists because there's no good way
+  // of observing browser autofill.
+  setInterval(function() {
+    $('.form_v2 .field:not(:focus)').each(function() {
+      var $elem = $(this);
+      var value = $elem.val();
+      if (value && value != '') {
+        $elem.trigger('blur')
+      };
+    })
+  }, 250);
+
+  $('.form_v2 .field').on('focus', function() {
+    $(this).closest('.fieldwrap').addClass('filled');
+  });
+
+  $('.form_v2 .field_inline').on('focus', function() {
+    $(this).addClass('filled');
+  });
+
+  $('.form_v2 .field').on('blur', function() {
+    var $elem = $(this);
+    var value = $elem.val();
+    if (!value || $.trim(value) == '') {
+      $elem.closest('.fieldwrap').removeClass('filled valid');
+    } else {
+      $elem.closest('.fieldwrap').addClass('filled valid');
+    }
+  });
+
   window.SignupPage = Backbone.View.extend({
   });
 
