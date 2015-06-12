22ae8af257853ed81bdcb0e7ce1ab03f16336b2f
Justin Reese <justin@justinreese.com>
2015-06-12 10:44:53 -0500

    Improve UX of signup form, add journo toggling
    There is altogether way too much markup and styling here.

--- public/javascripts/app/signup.js
+++ public/javascripts/app/signup.js
@@ -15,6 +15,28 @@ $(function() {
     })
   }, 250);
 
+  $('input[name="is_journalism"]').on('change', function(){
+    if ($(this).val() == 'yes') {
+      var $on  = $('#journalism_fields');
+      var $off = $('#nonjournalism_fields');
+    } else {
+      var $on  = $('#nonjournalism_fields');
+      var $off = $('#journalism_fields');
+    }
+    $on.removeClass('closed').addClass('open').find('input').removeAttr('tabindex');
+    $off.removeClass('open').addClass('closed').find('input').attr('tabindex', '-1');;
+  });
+
+  $('input[name="approver"]').on('change', function(){
+    $inputs = $('label[for="approver_other"]').find('input');
+    if ($(this).val() == 'other') {
+      $inputs.prop('disabled', false);
+      $inputs.first().focus();
+    } else {
+      $inputs.prop('disabled', true);
+    }
+  });
+
   $('.form_v2 .field').on('focus', function() {
     $(this).closest('.fieldwrap').addClass('filled');
   });
@@ -25,7 +47,7 @@ $(function() {
     if (!value || $.trim(value) == '') {
       $elem.closest('.fieldwrap').removeClass('filled valid');
     } else {
-      $elem.closest('.fieldwrap').addClass('filled valid');
+      $elem.closest('.fieldwrap').addClass('filled valid').removeClass('invalid');
     }
   });
 
