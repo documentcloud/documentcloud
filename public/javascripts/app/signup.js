e712e7c5e6b0df610d23c544574620f65fe0bf77
Justin Reese <justin@justinreese.com>
2015-06-15 17:54:45 -0500

    Begin signup form validation

--- public/javascripts/app/signup.js
+++ public/javascripts/app/signup.js
@@ -1,14 +1,78 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+SignupFormModel = Backbone.Model.extend({
+
+  VALIDATORS: {
+    isntBlank: function(val) {
+      var valid = Backbone.$.trim(val) != '';
+      return valid || "Please enter a value.";
+    },
+    isEmail: function(val) {
+      var pattern = dc.app.validator.email;
+      var valid = pattern.test(val);
+      return valid || "Please enter a valid email.";
+    },
+    isTrue: function(val) {
+      var valid = !!val;
+      return valid || "Must be selected.";
+    },
+  },
+
+  VALIDATIONS: {
+    'requester_email':      ['isEmail'],
+    'requester_first_name': ['isntBlank'],
+    'requester_last_name':  ['isntBlank'],
+    'organization_name':    ['isntBlank'],
+    'country':              ['isntBlank'],
+    'is_journalist':        ['isTrue'],
+    'agreed_to_terms':      ['isTrue'],
+  },
+
+  validateField: function(model, fieldName, value){
+    
+  },
+
+  validate: function(attrs, options) {
+    var errors = {};
+
+    if (options.only) {
+      var attr = options.only;
+      _.each(this.VALIDATIONS[attr], function(validator) {
+        var valid = this.VALIDATORS[validator](attrs[attr]);
+        if (valid !== true) { (errors[attr] = (errors[attr] || [])).push(valid); }
+      }, this);
+    } else {
+      _.each(this.VALIDATIONS, function(validators, attr) {
+        _.each(validators, function(validator) {
+          var valid = this.VALIDATORS[validator](this.get(attr));
+          if (valid !== true) { (errors[attr] = (errors[attr] || [])).push(valid); }
+        }, this);
+      }, this);
+    }
+
+    if (!_.isEmpty(errors)) {
+      return errors;
+    }
+  },
+
+  url: function() {
+    return '/signup';
+  },
+
+  initialize: function() {
+  },
+});
+
 SignupFormView = Backbone.View.extend({
   
   events: {
-    'focus .field':                       'enableFieldFocus',
+    'focus .field':                       'toggleFieldFocus',
+    'blur .field':                        'toggleFieldFocus',
     'change .field':                      'checkForUserInput',
-    'blur .field':                        'checkForUserInput',
     'change input[name="is_journalist"]': 'checkIsJournalist',
     'change input[name="approver"]':      'checkIsApprover',
+    'submit':                             'submit',
   },
   
   initialize: function(options) {
@@ -19,39 +83,86 @@ SignupFormView = Backbone.View.extend({
     
   },
   
-  enableFieldFocus: function(event) {
-    var $target = $(event.target);
-    $target.closest('.fieldwrap').addClass('filled');
+  submit: function(event){
+    event.preventDefault();
+
+    // TODO: Disable form before XHR
+
+    var saveForm = this.model.save({}, {
+      success: function() {
+        // Validation succeeded and XHR worked
+        // TODO: Move on to thanks page
+        console.log('Server happy');
+      },
+      error: function() {
+        // Validation succeeded but XHR failed
+        // TODO: Display global error/instrux
+        console.log('Server sad');
+      },
+    });
+
+    // TODO: Re-enable form after XHR
+
+    if (!saveForm) {
+      // Validation failure, XHR (was) halted
+      console.log(this.model.validationError);
+      this.displayValidationErrors();
+    }
+  },
+
+  displayValidationErrors: function() {
+    _.each(this.model.validationError, function(messages, attr) {
+      this.$('[name="' + attr + '"]').closest('.fieldwrap, .checkwrap, .radioset').addClass('invalid');
+    }, this);
+  },
+  
+  toggleFieldFocus: function(event) {
+    var $target = this.$(event.target);
+    var value   = $target.val();
+    if ($target.is(':focus') || value) {
+      $target.closest('.w').addClass('filled');
+    } else {
+      $target.closest('.fieldwrap').removeClass('filled');
+    }
   },
 
   checkForUserInput: function(event) {
-    var $target = $(event.target);
+    var $target = this.$(event.target);
+    var attr    = $target.attr('name');
     var value   = $target.val();
-    // TODO: Figure out why our Backbone won't allow simply `set(key, val)`
-    var attrs = {};
-    attrs[$target.attr('name')] = value;
-    this.model.set(attrs);
-    if (!value || $.trim(value) == '') {
-      $target.closest('.fieldwrap').removeClass('filled');
-    } else {
-      $target.closest('.fieldwrap').addClass('filled');
+
+    this.toggleFieldFocus(event);
+
+    // Don't set the model if we're just tabbing through
+    if (value || this.model.get(attr)) {
+      // TODO: Figure out why our Backbone won't allow simply `set(key, val)`
+      var attrs = {};
+      attrs[$target.attr('name')] = value;
+      this.model.set(attrs, { validate: true, only: attr });
+
+      var $fieldwrap = $target.closest('.fieldwrap, .checkwrap, .radioset');
+      if (this.model.validationError) {
+        $fieldwrap.addClass('invalid').removeClass('valid');
+      } else {
+        $fieldwrap.addClass('valid').removeClass('invalid');
+      }
     }
   },
 
-  validateUserInput: function(event) {
-    var $target = $(event.target);
-    var value   = $target.val();
-    if (value) {
-      $target.closest('.fieldwrap').addClass('valid').removeClass('invalid');
-    } else {
-      $target.closest('.fieldwrap').addClass('invalid').removeClass('valid');
-    }
-  },
+  // validateUserInput: function(event) {
+  //   var $target = this.$(event.target);
+  //   var value   = $target.val();
+  //   if (value) {
+  //     $target.closest('.fieldwrap').addClass('valid').removeClass('invalid');
+  //   } else {
+  //     $target.closest('.fieldwrap').addClass('invalid').removeClass('valid');
+  //   }
+  // },
 
   checkIsJournalist: function(event) {
-    var $target = $(event.target);
-    var $journo_fields    = $('#journalism_fields');
-    var $nonjourno_fields = $('#nonjournalism_fields');
+    var $target = this.$(event.target);
+    var $journo_fields    = this.$('#journalism_fields');
+    var $nonjourno_fields = this.$('#nonjournalism_fields');
 
     if ($target.val() == 'yes') {
       var $on  = $journo_fields;
@@ -65,9 +176,9 @@ SignupFormView = Backbone.View.extend({
   },
 
   checkIsApprover: function(event) {
-    var $target = $(event.target);
+    var $target = this.$(event.target);
 
-    var $inputs = this.$el.find('label[for="approver_other"]').find('.field');
+    var $inputs = this.$('label[for="approver_other"]').find('.field');
     if ($target.val() == 'other') {
       $inputs.prop('disabled', false);
       $inputs.first().focus();
@@ -78,6 +189,6 @@ SignupFormView = Backbone.View.extend({
 });
 
 $(function() {
-  window.signupModel = new Backbone.Model({});
+  window.signupModel = new SignupFormModel({});
   window.signup = new SignupFormView({ model: signupModel, el: $('#new_verification_request') });
 });
