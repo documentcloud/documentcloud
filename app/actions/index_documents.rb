a3de657a99bd4c134f33ee3737c7bdb2921b160c
Ted Han <ted@knowtheory.net>
2015-07-12 09:45:23 -0500

    Forgot to rework elasticsearch indexer.

--- app/actions/index_documents.rb
+++ app/actions/index_documents.rb
@@ -3,17 +3,17 @@ require File.dirname(__FILE__) + '/support/setup'
 class IndexDocuments < CloudCrowd::Action
   
   def process
-    Document.where(id: input).each{ |doc| DC::Search.repository.save(doc) }
+    Document.where(id: input).each{ |doc| DC::Search.repository(doc.class).save(doc) }
     true
   end
 end
 
 __END__
 counter = 0
-Document.pluck(:id).each_slice(5000) do |ids|
+Document.limit(10000).pluck(:id).each_slice(5000) do |ids|
   counter+=1
-  RestClient.post(DC::CONFIG['cloud_crowd_server'] + '/jobs', {:job => { 
-    :action => 'elastic_index', 
+  RestClient.post(DC::SECRETS['central_server'] + '/jobs', {:job => { 
+    :action => 'index_documents', 
     :inputs => [ids]
   }.to_json})
   puts counter
