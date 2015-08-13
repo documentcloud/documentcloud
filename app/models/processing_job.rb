e5ceb40df377187272ec70b041a47c6ad9d854d8
Ted Han <ted@knowtheory.net>
2015-08-13 12:20:40 -0400

    Notify communication failures w/ CloudCrowd

--- app/models/processing_job.rb
+++ app/models/processing_job.rb
@@ -68,6 +68,7 @@ class ProcessingJob < ActiveRecord::Base
       # We've collected all the info we need, so
       save # it and retain the lock on the document.
     rescue Errno::ECONNREFUSED, RestClient::Exception => error
+      LifecycleMailer.exception_notification(e).deliver
       # In the event of an error while communicating with CloudCrowd, unlock the document.
       self.update_attributes :complete => true
       #document.update :status => AVAILABLE
