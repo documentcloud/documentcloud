2c25efdd8f41d8380a0738c9564e779d11417829
Justin Reese <justin@justinreese.com>
2015-08-17 13:50:37 -0500

    Move account request functionality out of platform

--- config/routes.rb
+++ config/routes.rb
@@ -130,9 +130,8 @@ DC::Application.routes.draw do
   match '/accounts/enable/:key' => 'accounts#enable', :via => [:get, :post], :as => :enable_account
   match '/reset_password' => 'accounts#reset', :via => [:get, :post], :as => :reset_password
 
-  get  '/signup' => 'redirect#index', :url => '/apply'
-  get  '/apply' => 'signup#index', :as => :apply
-  post '/apply' => 'signup#create', :as => :apply_create
+  get  '/signup' => 'redirect#index', :url => '/plans/apply'
+  get  '/apply'  => 'redirect#index', :url => '/plans/apply'
 
   # Organizations management
   resources :organizations, :only=>:update
