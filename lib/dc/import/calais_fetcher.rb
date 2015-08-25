ebbd5b19083adae133cf3ff1f9f58208c1421585
Ted Han <ted@knowtheory.net>
2015-08-25 11:50:51 -0400

    Rearrange CalaisFetcher to use DC::SECRETS for keys, transaction for updating call counter, identify flaw in OpenCalais gem.

--- lib/dc/import/calais_fetcher.rb
+++ lib/dc/import/calais_fetcher.rb
@@ -9,6 +9,7 @@ module DC
       # safety padding because Calais was still throwing errors. Perhaps they're
       # counting bytes?
       MAX_TEXT_SIZE = 95000
+      API_LIMIT     = DC::SECRETS['calais_api_limit']
 
       # Fetch the RDF from OpenCalais, splitting it into chunks small enough
       # for Calais to swallow. Run the chunks in parallel.
@@ -38,14 +39,19 @@ module DC
         text.gsub!(/<\/?[^>]*>/) {|m| ' ' * m.length }
         retry_calais_errors do
           # Increment the count of our API calls
-          current_value = AppConstant.value(OPEN_CALAIS_KEY).to_i
+          current_value = AppConstant.value("calais_calls_made").to_i
           # If we are over the max calls allowed, blacklist the action
-          if current_value >= OPEN_CALAIS_MAX_DAILY_CALLS
-            RestClient.post DC::CONFIG['cloud_crowd_server'] + '/blacklist', {name: "reprocess_entities"}
+          if current_value < API_LIMIT
+            client = OpenCalais::Client.new(:api_key=>DC::SECRETS['calais_license'])
+            analyzed_text = client.analyze(text, :exact => false)
+            AppConstant.transaction do
+              current_value = AppConstant.value("calais_calls_made").to_i
+              AppConstant.replace("calais_calls_made", current_value + 1)
+            end
+            return analyzed_text
           else
-            client = OpenCalais::Client.new(:api_key=>DC::SECRETS['calais_license_v2'])
-            client.enrich(text)
-            AppConstant.replace(OPEN_CALAIS_KEY, current_value + 1)
+            RestClient.post DC::CONFIG['cloud_crowd_server'] + '/blacklist', {action: "reprocess_entities"}
+            false
           end
         end
       end
@@ -56,7 +62,7 @@ module DC
         attempts = 0
         begin
           yield
-        rescue Calais::Error, Curl::Err::CurlError, Timeout::Error => e
+        rescue OpenCalais::Error, Curl::Err::CurlError, Timeout::Error => e
           Rails.logger.warn e.message
           return nil if e.message == 'Calais continues to expand its list of supported languages, but does not yet support your submitted content.'
           Rails.logger.warn 'waiting 10 seconds'
