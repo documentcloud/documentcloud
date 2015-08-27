3014b8aa92947d1afc837e9e2f5742fa8079179f
Ted Han <ted@knowtheory.net>
2015-08-27 19:09:31 -0400

    Cache invalidation code for CloudFront

--- lib/dc/store/aws_s3_store.rb
+++ lib/dc/store/aws_s3_store.rb
@@ -195,23 +195,22 @@ module DC
       
       def invalidate_cloudfront_cache(document)
         if distribution_id = DC::SECRETS['cloudfront_distribution_id']
-          arr = (1..document.page_count).map do |page_number|
+          paths = (1..document.page_count).map do |page_number|
             page_image_paths = Page::IMAGE_SIZES.keys.map do |size|
               document.page_image_path(page_number, size)
             end
             [document.page_text_path(page_number)] + page_image_paths
           end.flatten
           
-          arr.push document.pdf_path
-          arr.push document.full_text_path
-          arr.push document.original_file_path unless document.original_file_path == document.pdf_path
-          arr
+          paths.push document.pdf_path
+          paths.push document.full_text_path
+          paths.push document.original_file_path unless document.original_file_path == document.pdf_path
           
           cloudfront.client.create_invalidation({
             :distribution_id    => distribution_id,
             :invalidation_batch => {
-              :paths            => { :quantity => paths.size, :items => paths },
-              :caller_reference => document.id
+              :paths            => { :quantity => paths.size, :items => paths.map{|p| "/#{p}"} },
+              :caller_reference => document.id.to_s
             }
           })
         else 
