3882c89aba7b2087856084a53ae8b06be7aea81f
Justin Reese <justin@justinreese.com>
2015-12-02 17:51:05 -0600

    Comment and clean up embed controller

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -5,12 +5,17 @@ class EmbedController < ApplicationController
   # In real life, these are only accessed from the CDN root.
   before_action { not_found } unless Rails.env.development? || Rails.env.test?
 
+  # The enhancer is designed in `documentcloud-pages`, installed via bower, 
+  # built by `rake build:embed:page`, and deployed to S3, but but we provide a 
+  # route to it here for development and testing.
   def enhance
     return not_implemented unless request.format.js?
 
-    render :enhance, :content_type => Mime::Type.lookup_by_extension('js')
+    render :enhance, :content_type => js_mime_type
   end
 
+  # The loaders live in their respective view folders and are deployed to S3,
+  # but we provide routes to them here for development and testing.
   def loader
     return not_implemented unless request.format.js?
 
@@ -20,12 +25,15 @@ class EmbedController < ApplicationController
       'embed'     => 'search'
     }
 
-    if matching_controller = object_controller_map[params[:object]]
-      render "#{matching_controller}/embed_loader",
-             :content_type => Mime::Type.lookup_by_extension('js')
-    else
-      return not_found
-    end
+    return not_found unless matching_controller = object_controller_map[params[:object]]
+
+    render "#{matching_controller}/embed_loader", :content_type => js_mime_type
+  end
+
+  private
+  
+  def js_mime_type
+    Mime::Type.lookup_by_extension('js')
   end
 
 end
