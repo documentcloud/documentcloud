cde6ead0d520a4ab944ac2c81dda3714acd0896b
Justin Reese <justin@justinreese.com>
2015-12-21 18:02:17 -0600

    Test we aren't slugging in 21-bit unicode space

--- test/lib/sanitized_test.rb
+++ test/lib/sanitized_test.rb
@@ -4,6 +4,13 @@ class SanitizedTest < ActiveSupport::TestCase
   include DC::Sanitized
   
   it "should strip non-characters & replace them with dashes" do
-    assert sluggify(%[''(asdf)'''''___⌘⌘⌘\n"lol.wat".。---不]), "asdf-lol-wat-不"
+    assert_equal "asdf-lol-wat-不", sluggify(%[''(asdf)'''''___⌘⌘⌘\n"lol.wat".。---不])
   end
+
+  # Make sure characters from outside our intended unicode range are being 
+  # removed, as supporting them requires changes to our non-Ruby patterns
+  it "should not support full 21-bit Unicode dictionary" do
+    assert_equal "hi-kitty", sluggify("hi kitty 😺")
+  end
+
 end
