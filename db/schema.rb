9c96f1b7c55a35ec8d9749e3e2215d2715b07a31
Ted Han <ted@knowtheory.net>
2016-01-20 20:26:00 -0800

    Add an aspect ratio field to pages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111214857) do
+ActiveRecord::Schema.define(version: 20160120214245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -183,6 +183,7 @@ ActiveRecord::Schema.define(version: 20151111214857) do
     t.text    "text",            null: false
     t.integer "start_offset"
     t.integer "end_offset"
+    t.float   "aspect_ratio"
   end
 
   add_index "pages", ["document_id"], name: "index_pages_on_document_id", using: :btree
