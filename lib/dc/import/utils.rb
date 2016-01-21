146ee2eaead6df93667412ae65105f7fc55ca166
Justin Reese <justin@justinreese.com>
2016-01-21 02:27:37 -0600

    More fixes, more Ruby to `DC::Import::Utils`

--- lib/dc/import/utils.rb
+++ lib/dc/import/utils.rb
@@ -8,7 +8,8 @@ module DC
       end
 
       def self.save_page_images(asset_store, document, page_number, filename, access)
-        images = Page::IMAGE_SIZES.reduce({}) do |sizes, label, size|
+        images = Page::IMAGE_SIZES.reduce({}) do |sizes, label_size_pair|
+          label, size  = label_size_pair
           sizes[label] = "images/#{size}/#{filename}"
           sizes
         end
