03954d44b6a6a160bfcb047569667b6ea56fb510
Ted Han <ted@knowtheory.net>
2016-01-27 13:30:29 -0800

    Add a temporary guard for duplicate documents which don't have page aspect ratios calculated.

--- app/actions/document_import.rb
+++ app/actions/document_import.rb
@@ -84,7 +84,7 @@ class DocumentImport < DocumentAction
 
   def process_images
     if duplicate = document.duplicates.first
-      @page_aspect_ratios = duplicate.pages.order(:page_number).pluck(:aspect_ratio)
+      @page_aspect_ratios = duplicate.pages.order(:page_number).pluck(:aspect_ratio).map{ |n| n.nil? 0 : n }
       asset_store.copy_images( duplicate, document, access )
     else
       Docsplit.extract_images(@pdf, :format => :gif, :size => Page::IMAGE_SIZES.values, :rolling => true, :output => 'images')
