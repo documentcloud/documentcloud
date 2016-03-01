147ac05207c9383ec7950c0240e3fd11bc86c7fe
Justin Reese <justin@justinreese.com>
2016-03-01 17:52:51 -0600

    Clean up route formatting
    * Disarm hashrockets
    * Convert symbols to strings (arguably wrong, but at least consistently wrong) except array values and a handful of `scope` and `resource` params
    * Use `do/end` for all blocks
    * Clean up comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,47 @@
 DC::Application.routes.draw do
 
-  # homepage
-  get '/' => 'workspace#index'
+  # Homepage
+  get '/', to: 'workspace#index'
 
-  get '/embed/loader/enhance', as: :embed_enhance, to: 'embed#enhance'
-  get '/:object/loader',       as: :embed_loader,  to: 'embed#loader', object: /viewer|notes|embed/
+  # Embed loaders
+  get '/embed/loader/enhance', to: 'embed#enhance', as: 'embed_enhance'
+  get '/:object/loader',       to: 'embed#loader',  as: 'embed_loader', object: /viewer|notes|embed/
 
-  # Internal Search API.
-  get '/search/documents.json' => 'search#documents'
+  # Internal search API
+  get '/search/documents.json', to: 'search#documents'
 
-  # Search Embeds.
-  get '/search/embed/:q/:options.:format' => 'search#embed', :q => /[^\/;,?]*/, :options => /p-(\d+)-per-(\d+)-order-(\w+)-org-(\d+)(-secure)?/
-  get '/search/embed/:options.:format' => 'search#embed',    :q => /[^\/;,?]*/, :options => /p-(\d+)-per-(\d+)-order-(\w+)-org-(\d+)(-secure)?/
-  # Journalist workspace and surrounding HTML.
-  get '/search' => 'workspace#index'
-  get '/search/preview' => 'search#preview', :as => :preview
-  get '/search/restricted_count' => 'search#restricted_count'
-  get '/search/:query' => 'workspace#index', :query => /.*/
-  get '/help' => 'workspace#help'
-  get '/help/:page' => 'workspace#help'
-  get '/results' => 'workspace#index', :as => :results
+  # Search embeds
+  get '/search/embed/:q/:options.:format', to: 'search#embed', q: /[^\/;,?]*/, options: /p-(\d+)-per-(\d+)-order-(\w+)-org-(\d+)(-secure)?/
+  get '/search/embed/:options.:format',    to: 'search#embed', q: /[^\/;,?]*/, options: /p-(\d+)-per-(\d+)-order-(\w+)-org-(\d+)(-secure)?/
 
-  # login / logout
-  scope( controller: 'authentication' ) do
-    match '/login',                       action: 'login', :via=>[:get,:post]
+  # Journalist workspace and surrounding HTML
+  get '/search',                  to: 'workspace#index'
+  get '/search/preview',          to: 'search#preview', as: 'preview'
+  get '/search/restricted_count', to: 'search#restricted_count'
+  get '/search/:query',           to: 'workspace#index', query: /.*/
+  get '/help',                    to: 'workspace#help'
+  get '/help/:page',              to: 'workspace#help'
+  get '/results',                 to: 'workspace#index', as: 'results'
+
+  # Authentication
+  scope(controller: 'authentication') do
+    match '/login',                       action: 'login', via: [:get, :post]
     get '/logout',                        action: 'logout'
     get '/auth/remote_data/:document_id', action: 'remote_data'
 
     # Third party auth via OmniAuth
-    match '/auth/:action', :via=>[:get,:post]
-    get '/auth/:provider',          :action => :blank
-    get '/auth/:provider/callback', :action => :callback
+    match '/auth/:action', via: [:get, :post]
+    get '/auth/:provider',          action: 'blank'
+    get '/auth/:provider/callback', action: 'callback'
   end
 
-  # Public search.
-  get '/public/search' => 'public#index', :as => :public_search
-  get '/public/search/:query' => 'public#index', :query => /.*/
+  # Public search
+  get '/public/search',        to: 'public#index', as: 'public_search'
+  get '/public/search/:query', to: 'public#index', query: /.*/
 
-  # API.
-  scope( 'api', controller: 'api' ) do
-    scope( via: :options, action: 'cors_options' ) do
+  # API
+  scope(:api, controller: 'api') do
+    scope(via: 'options', action: 'cors_options') do
       match 'documents/pending.:format',            allowed_methods: [:get]
       match 'documents/:id.:format',                allowed_methods: [:get, :put, :delete]
       match 'documents/:id/entities.:format',       allowed_methods: [:get]
@@ -64,16 +66,20 @@ DC::Application.routes.draw do
   end
 
   resources :featured do
-    collection { post :present_order }
+    collection do
+      post 'present_order'
+    end
   end
 
   resources :documents do
 
     resources :annotations do
-      member {  match '(*all)', action: :cors_options, via: :options, allowed_methods: [:put,:delete,:post] }
+      member do
+        match '(*all)', action: 'cors_options', via: 'options', allowed_methods: [:put, :delete, :post]
+      end
     end
     resource :annotation do
-      match '(*all)', action: :cors_options, via: :options, allowed_methods: [:get,:post]
+      match   '(*all)', action: 'cors_options', via: 'options', allowed_methods: [:get, :post]
     end
     
     # in order not to clobber existing routes
@@ -83,114 +89,116 @@ DC::Application.routes.draw do
     # untangle `params[:page_number|:id|:document_id]` in `PagesController`
     
     collection do
-      get :status
-      get :queue_length
-      get :preview
-      get :published
-      get :entity
-      get :unpublished
-      get :entities
-      get :dates
-      get :occurrence
+      get 'status'
+      get 'queue_length'
+      get 'preview'
+      get 'published'
+      get 'entity'
+      get 'unpublished'
+      get 'entities'
+      get 'dates'
+      get 'occurrence'
     end
     member do
-      post :upload_insert_document
-      post :reprocess_text
-      post :remove_pages
-      get  :search
-      get  :per_page_note_counts
-      post :redact_pages
-      get  :mentions
-      post :reorder_pages
-      post :save_page_text
-      get  :preview
-      get  'pages/:page_name.txt', :action=>:send_page_text
-      post 'pages/:page_name.txt', :action=>:set_page_text
-      get  'pages/:page_name.gif', :action=>:send_page_image
+      post 'upload_insert_document'
+      post 'reprocess_text'
+      post 'remove_pages'
+      get  'search'
+      get  'per_page_note_counts'
+      post 'redact_pages'
+      get  'mentions'
+      post 'reorder_pages'
+      post 'save_page_text'
+      get  'preview'
+      get  ':slug.pdf',            action: 'send_pdf'
+      get  ':slug.txt',            action: 'send_full_text'
+      get  'pages/:page_name.txt', action: 'send_page_text'
+      post 'pages/:page_name.txt', action: 'set_page_text'
+      get  'pages/:page_name.gif', action: 'send_page_image'
       get  'pages/:page_number.:format', to: "pages#show"
-      get  ':slug.pdf', :action=>:send_pdf
-      get  ':slug.txt', :action=>:send_full_text
     end
   end
 
-  # # Print notes.
-  get '/notes/print' => 'annotations#print', :as => :print_notes
+  # # Print notes
+  get '/notes/print', to: 'annotations#print', as: 'print_notes'
 
-  # Reviewers.
+  # Reviewers
   resources :reviewers do
-    collection {
-      post :send_email
-      get :preview_email
-    }
+    collection do
+      post 'send_email'
+      get  'preview_email'
+    end
   end
 
-  # Bulk downloads.
-  get '/download/*args.zip' => 'download#bulk_download', :as => :bulk_download
+  # Bulk downloads
+  get '/download/*args.zip', to: 'download#bulk_download', as: 'bulk_download'
 
-  # Accounts and account management.
+  # Accounts and account management
   resources :accounts do
-    collection {
-      get :logged_in
-    }
-    member {
-      post :resend_welcome
-    }
+    collection do
+      get 'logged_in'
+    end
+    member do
+      post 'resend_welcome'
+    end
   end
-  match '/accounts/enable/:key' => 'accounts#enable', :via => [:get, :post], :as => :enable_account
-  match '/reset_password' => 'accounts#reset', :via => [:get, :post], :as => :reset_password
+  match '/accounts/enable/:key', to: 'accounts#enable', via: [:get, :post], as: 'enable_account'
+  match '/reset_password',       to: 'accounts#reset',  via: [:get, :post], as: 'reset_password'
 
-  get  '/signup' => 'redirect#index', :url => '/plans/apply'
-  get  '/apply'  => 'redirect#index', :url => '/plans/apply'
+  # Account requests
+  get '/signup', to: 'redirect#index', url: '/plans/apply'
+  get '/apply',  to: 'redirect#index', url: '/plans/apply'
 
   # Organizations management
-  resources :organizations, :only=>:update
+  resources :organizations, only: [:update]
 
-  # Projects.
+  # Projects
   resources :projects do
-    member {
-      post :add_documents
-      post :remove_documents
-      get :documents
-    }
-    resources :collaborators, :only=>[:create,:destroy]
+    member do
+      post 'add_documents'
+      post 'remove_documents'
+      get  'documents'
+    end
+    resources :collaborators, only: [:create, :destroy]
   end
 
-  # Home pages.
-  get '/contributors'  => 'home#contributors',  :as => :contributors
-  get '/faq'           => 'home#faq'
-  get '/terms'         => 'home#terms',         :as => :terms
-  get '/privacy'       => 'home#privacy',       :as => :privacy
-  get '/p3p.:format'   => 'home#p3p',           :as => :p3p
-  get '/home'          => 'home#index',         :as => :home
-  get '/opensource'    => 'home#opensource',    :as => :opensource
-  get '/about'         => 'home#about',         :as => :about
-  get '/contact'       => 'home#contact',       :as => :contact
-  get '/help'          => 'home#help'
-  get '/help/:page'    => 'home#help'
-  get '/multilanguage' => 'home#multilanguage', :as => :multilanguage
+  # Home pages
+  get '/contributors',  to: 'home#contributors',  as: 'contributors'
+  get '/faq',           to: 'home#faq'
+  get '/terms',         to: 'home#terms',         as: 'terms'
+  get '/privacy',       to: 'home#privacy',       as: 'privacy'
+  get '/p3p.:format',   to: 'home#p3p',           as: 'p3p'
+  get '/home',          to: 'home#index',         as: 'home'
+  get '/opensource',    to: 'home#opensource',    as: 'opensource'
+  get '/about',         to: 'home#about',         as: 'about'
+  get '/contact',       to: 'home#contact',       as: 'contact'
+  get '/help',          to: 'home#help'
+  get '/help/:page',    to: 'home#help'
+  get '/multilanguage', to: 'home#multilanguage', as: 'multilanguage'
 
-  # Redirects.
-  get '/index.php'             => 'redirect#index',                     :url => '/'
-  get '/document-contributors' => 'redirect#index',                     :url => '/contributors'
-  get '/faq.php'               => 'redirect#index',                     :url => '/faq'
-  get '/who.php'               => 'redirect#index', :as => :who,        :url => '/about'
-  get '/who-we-are'            => 'redirect#index', :as => :who_we_are, :url => '/about'
-  get '/partner.php'           => 'redirect#index', :as => :partner,    :url => '/contributors'
-  get '/blog/feed'             => 'redirect#index', :as => :feed,       :url => 'https://blog.documentcloud.org/feed'
-  get '/feed'                  => 'redirect#index', :as => :root_feed,  :url => 'https://blog.documentcloud.org/feed'
-  get '/blog/*parts'           => 'redirect#index', :as => :blog,       :url => 'https://blog.documentcloud.org/'
-  get '/clips.php'             => 'redirect#index', :as => :clips,      :url => 'https://blog.documentcloud.org/'
-  get '/news'                  => 'redirect#index', :as => :news,       :url => 'https://blog.documentcloud.org/'
-  get '/blog'                  => 'redirect#index',                     :url => 'https://blog.documentcloud.org/'
+  # Redirects
+  get '/index.php',             to: 'redirect#index',                     url: '/'
+  get '/document-contributors', to: 'redirect#index',                     url: '/contributors'
+  get '/faq.php',               to: 'redirect#index',                     url: '/faq'
+  get '/who.php',               to: 'redirect#index', as: 'who',          url: '/about'
+  get '/who-we-are',            to: 'redirect#index', as: 'who_we_are',   url: '/about'
+  get '/partner.php',           to: 'redirect#index', as: 'partner',      url: '/contributors'
+  get '/blog/feed',             to: 'redirect#index', as: 'feed',         url: 'https://blog.documentcloud.org/feed'
+  get '/feed',                  to: 'redirect#index', as: 'root_feed',    url: 'https://blog.documentcloud.org/feed'
+  get '/blog/*parts',           to: 'redirect#index', as: 'blog',         url: 'https://blog.documentcloud.org/'
+  get '/clips.php',             to: 'redirect#index', as: 'clips',        url: 'https://blog.documentcloud.org/'
+  get '/news',                  to: 'redirect#index', as: 'news',         url: 'https://blog.documentcloud.org/'
+  get '/blog',                  to: 'redirect#index',                     url: 'https://blog.documentcloud.org/'
 
-  get '/admin' => 'admin#index'
+  # Admin section
+  get '/admin', to: 'admin#index'
   get '/admin/health_check/:subject/:env', to: 'admin#health_check', subject: /page_embed/, env: /production|staging/
   
   # Standard fallback routes
-  match '/:controller(/:action(/:id))', :via=>[:get,:post]
+  match '/:controller(/:action(/:id))', via: [:get, :post]
   get ':controller/:action.:format'
 
 end
 
-# Magic invocation to ask engines to add their routes.
+# Magic invocation to ask engines to add their routes
 Rails.application.load_additional_routes
