cc09320e676b97a1ca728617ead521b02d1bb624
Ted Han <ted@knowtheory.net>
2016-03-01 15:14:48 -0800

    Make sure hstore is active

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20160129000705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "accounts", force: :cascade do |t|
     t.string   "first_name",        limit: 40
