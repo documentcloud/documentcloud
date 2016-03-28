8dec5f16a8dfcb23a0c6ed230e01f40cbbbe958e
Ted Han <ted@knowtheory.net>
2016-03-28 13:34:53 -0700

    Make the cache only max-age not shared max age.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,7 +64,7 @@ class ApplicationController < ActionController::Base
     when statement
       statement
     when cachable?
-      "public, max-age=5, s-maxage=5"
+      "public, max-age=5"
     else
       "no-cache"
     end
