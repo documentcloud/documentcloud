d626792340ac012142a7a289490f7bc34e048e93
Justin Reese <justin@justinreese.com>
2016-04-04 12:58:05 -0500

    Hotfix: Let authenticated users view public search
    Fixes #375

--- public/javascripts/app/workspace.js
+++ public/javascripts/app/workspace.js
@@ -13,7 +13,7 @@ dc.controllers.Workspace = Backbone.Router.extend({
     this.renderSubViews();
     dc.app.searcher = new dc.controllers.Searcher;
     if (!Backbone.history.start({pushState : true, root : dc.account ? '/' : '/public/'})) {
-      dc.app.searcher.loadDefault({showHelp: true});
+      dc.app.searcher.loadDefault({showHelp: !(dc.account && window.location.pathname.indexOf('/public/') === 0)});
     }
   },
 
