ec2e54b620ed6f11eabf70fb77dd0c514c644e6a
Ted Han <ted@knowtheory.net>
2016-05-07 14:18:55 -0700

    Test mapping for Broccoli tool chain.

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,18 @@
 var funnel      = require('broccoli-funnel');
-var mergeTrees  = require('broccoli-merge-trees');
+var mergeTrees  = require('broccoli-descriptive-merge-trees');
 var compileSass = require('broccoli-sass');
+var bower       = require('broccoli-bower');
+var uglify      = require('broccoli-uglify-js');
 
-module.exports = 'public'
+var appRoot = 'public';
+var app = funnel(appRoot, {
+  srcDir: 'javascripts',
+  destDir: 'javascripts'
+});
+
+var styles = funnel(appRoot, {
+  srcDir: 'stylesheets',
+  destDir: 'stylesheets'
+});
+
+module.exports = mergeTrees([app, styles]);
