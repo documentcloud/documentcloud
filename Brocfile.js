685a8a490d4f149ab12c0c18658a3b8f0974a951
Ted Han <ted@knowtheory.net>
2016-05-09 07:51:49 -0700

    Sample Brocfile.

--- Brocfile.js
+++ Brocfile.js
@@ -4,15 +4,24 @@ var compileSass = require('broccoli-sass');
 var bower       = require('broccoli-bower');
 var uglify      = require('broccoli-uglify-js');
 
-var appRoot = 'public';
+var appRoot = 'public'; // Set up the base directory for all of our source files
+
+// Collect all of our javascripts and map them into an output directory called "javascripts"
 var app = funnel(appRoot, {
   srcDir: 'javascripts',
   destDir: 'javascripts'
 });
 
-var styles = funnel(appRoot, {
+// Collect all of our stylesheets and map them into an output directory called "stylesheets"
+var css = funnel(appRoot, {
   srcDir: 'stylesheets',
-  destDir: 'stylesheets'
+  destDir: 'stylesheets',
+  exclude: ["**/*.scss"]
 });
 
-module.exports = mergeTrees([app, styles]);
+// Compile master sass file into assets
+var sassDependencies = ['public'];
+var sass = compileSass(sassDependencies, 'stylesheets/v2/example.scss', 'stylesheets/v2/example.css');
+
+// coalesce all the different assets into one directory.
+module.exports = mergeTrees([app, css, sass]);
