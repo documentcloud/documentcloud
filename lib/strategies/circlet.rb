b1694ce0e68d9881e1f454ef0538d2bef8bea0eb
Esther Villars <esthervillars@gmail.com>
2016-05-24 14:47:25 -0700

    wip

---
+++ lib/strategies/circlet.rb
@@ -0,0 +1,28 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Circlet < OmniAuth::Strategies::OAuth2
+      #include OmniAuth::Strategy
+      # change the class name and the :name option to match your application name
+      option :name, :circlet
+
+      uid { raw_info["id"] }
+
+      info do
+        {
+          first_name: raw_info['first_name'],
+          last_name: raw_info['last_name'],
+          email: raw_info['email']
+          # and anything else you want to return to your API consumers
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/users/me.json').parsed
+      end
+    end
+  end
+end
+
+# OmniAuth.config.add_camelization 'circlet', 'Circlet'
