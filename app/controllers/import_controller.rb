829f83903b863d3b208627c0b4f93ac1bee4a978
Ted Han <ted@knowtheory.net>
2016-09-27 14:09:33 -0700

    First sketch for authentication for email uploader

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -22,9 +22,58 @@ class ImportController < ApplicationController
     end
   end
   
+  def upload_from_email
+    return forbidden unless correct_email_upload_secret?(params[:secret])
+    # take JSON blob
+    email = JSON.parse(params[:email])
+    uploader           = email[:from]
+    uploader_recipient = email[:to]
+    # 
+    # Query: How to validate that the email we have stored, and the password we have here
+    # are the same and both valid?
+    #
+    # Answer: 
+    #  -  process the email on the uploader side, and push it to a known layout, including the JSON blob.
+    #  - submit request with to/from pair & authenticate Rails request w/ that pair.
+    #  - initiate job which pulls and validates JSON blob from s3 (at known location) and uses that to
+    #    validate whether job should proceed or abort.
+    #
+    # user specifies FROM (the white list)
+    #
+    # we specify TO email address with the format :organization_slug-:key@upload.documentcloud.org
+    
+    # fetch message metadata
+    s3 = AWS::S3.new
+    bucket = s3.buckets['dc-email-uploads']
+    email_metadata_path = "emails/processed/#{email['id']}/#{email['id']}.json"
+    metadata = JSON.parse(bucket.objects[email_metadata_path].read)
+    recipients = metadata["to"]
+    sender = metadata["from"]
+    
+    # check for shenanigans and if someone is trying to send in json blobs for someone else's email
+    return forbidden unless sender == uploader
+    
+    # verify recipient email address
+    address_key = recipients.find do |address| 
+      address == uploader_recipient and address.split('@').last == 'upload.documentcloud.org'
+    end
+    
+    return forbidden unless address_key
+    # verify key against sender
+    key, domain = address_key.split('@')
+    
+    mailbox = UploadMailbox.lookup(sender, key)
+    return forbidden unless mailbox
+    
+    # Okay!  We're in the clear!  PROCEED WITH UPLOADS
+    
+    # get list of files
+    file_paths = email[:files]
+  end
+  
   # Returning a "201 Created" ack tells CloudCrowd to clean up the job.
   def cloud_crowd
-    return forbidden unless correct_cloud_crowd_secret?
+    return forbidden unless correct_cloud_crowd_secret?(params[:secret])
     cloud_crowd_job = JSON.parse(params[:job])
     if processing_job = ProcessingJob.lookup_by_remote(cloud_crowd_job)
       processing_job.resolve(cloud_crowd_job) do |pj| 
@@ -39,7 +88,7 @@ class ImportController < ApplicationController
   # CloudCrowd is done changing the document's asset access levels.
   # 201 created cleans up the job.
   def update_access
-    return forbidden unless correct_cloud_crowd_secret?
+    return forbidden unless correct_cloud_crowd_secret?(params[:secret])
     cloud_crowd_job = JSON.parse(params[:job])
     if processing_job = ProcessingJob.lookup_by_remote(cloud_crowd_job)
       processing_job.resolve(cloud_crowd_job) do |pj| 
@@ -52,8 +101,12 @@ class ImportController < ApplicationController
   
   private
   
-  def correct_cloud_crowd_secret?
-    params[:secret] == DC::SECRETS['cloud_crowd_secret']
+  def correct_email_upload_secret?(secret)
+    secret.kind_of? String and secret == DC::SECRETS['email_upload_secret']
+  end
+  
+  def correct_cloud_crowd_secret?(secret)
+    secret.kind_of? String and secret == DC::SECRETS['cloud_crowd_secret']
   end
 
   def expire_document_cache(document)
