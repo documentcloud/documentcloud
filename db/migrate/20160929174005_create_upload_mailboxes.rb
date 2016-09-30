b006b825decdb2ab80147979cb2d53e2d49ea198
Ted Han <ted@knowtheory.net>
2016-09-30 11:42:56 -0700

    Forgot a thing.

--- db/migrate/20160929174005_create_upload_mailboxes.rb
+++ db/migrate/20160929174005_create_upload_mailboxes.rb
@@ -4,6 +4,7 @@ class CreateUploadMailboxes < ActiveRecord::Migration
       t.integer :membership_id, null: false
       t.string  :sender,        limit: 255, null: false
       t.string  :recipient,     limit: 255, null: false
+      t.integer :upload_count,  default: 0
       t.timestamps null: false
     end
   end
