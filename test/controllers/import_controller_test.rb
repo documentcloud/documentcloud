86a8960f97f7357db9324e4610aa765dfc3dc09b
Justin Reese <justin@justinreese.com>
2016-10-05 14:43:50 -0500

    Clean up ImportControllerTest and add TODOs
    These are still failing and need fixing, but at least they look better.

--- test/controllers/import_controller_test.rb
+++ test/controllers/import_controller_test.rb
@@ -2,37 +2,45 @@ require 'test_helper'
 
 class ImportControllerTest < ActionController::TestCase
 
-  def test_upload_document
+  it "uploads a document if logged in" do
     get :upload_document
-    assert_response 403
+    assert_response :forbidden
 
     login_account!
     assert_difference 'Document.count' do
-      get :upload_document, :url=>'http://test.com/file.pdf', :file=>'on', :title=>"Test Doc"
+      get :upload_document, { url: 'http://test.com/file.pdf', file: 'on',
+                              title: "Test Doc" }
     end
     assert_response :success
     assert_job_action 'document_import'
   end
 
-  def test_cloud_crowd_success
-    ProcessingJob.create!( :document=>doc, :account=>louis, :cloud_crowd_id => 42, :title=>'import', :remote_job => 'document_import' )
-    assert_difference( ->{ ProcessingJob.incomplete.count }, -1 ) do
-      get :cloud_crowd, :job=>'{ "id": 42, "status":"succeeded" }'
+  # TODO: Fix this test! (I don't know how.)
+  it "cloud crowd success" do
+    ProcessingJob.create!(document: doc, account: louis, cloud_crowd_id: 42,
+      title: 'import', remote_job: 'document_import')
+    assert_difference(-> { ProcessingJob.incomplete.count }, -1) do
+      get :cloud_crowd, {job: {id: 42, status: 'succeeded'}.to_json}
     end
   end
 
-  def test_cloud_crowd_failure
-    ProcessingJob.create!( :document=>doc, :account=>louis, :cloud_crowd_id => 42,
-      :title=>'import', :remote_job => 'document_import' )
+  # TODO: Fix this test! (I don't know how.)
+  it "cloud crowd failure" do
+    ProcessingJob.create!(document: doc, account: louis, cloud_crowd_id: 42,
+      title: 'import', remote_job: 'document_import')
     assert_difference( ->{ ProcessingJob.incomplete.count }, -1 ) do
-      get :cloud_crowd, :job=>'{ "id": 42, "status":"failed" }'
+      get :cloud_crowd, {job: {id: 42, status: 'failed'}.to_json}
     end
     assert_equal Document::ERROR, doc.reload.access
   end
 
-  def test_update_access
-    get :update_access, {:job=>{'id'=>1234}.to_json}
-    assert_response 201
+  # TODO: Fix this test! (Need to pass a working secret to the second `get`)
+  it "allows updating access with a secret" do
+    get :update_access, {job: {id: 1234}.to_json}
+    assert_response :forbidden
+
+    get :update_access, {job: {id: 1234}.to_json, secret: 'foobar'}
+    assert_response :created
   end
 
 end
