fa72ebc51124e8125fbfa150f7297e00208e55eb
Justin Reese <justin@justinreese.com>
2016-11-18 15:57:57 -0600

    Wire up and process arbitrary donation amount

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -1,9 +1,53 @@
 class DonateController < ApplicationController
-
   layout 'new'
+
+  before_action :secure_only
 
   def index
     
+  end
+
+  def charge
+    @amount = params[:amount]
+    @amount = @amount.gsub('$', '').gsub(',', '')
+    begin
+      @amount = Float(@amount).round(2)
+    rescue
+      flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+      redirect_to donate_path
+      return
+    end
+    @amount = (@amount * 100).to_i # Cents
+
+    if @amount < 500
+      flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+      redirect_to donate_path
+      return
+    end
+      
+    begin
+      Stripe::Charge.create(
+        amount:      @amount,
+        currency:    'usd',
+        source:      params[:stripe_token],
+        description: 'DocumentCloud donation'
+      )
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to donate_path
+    end
+
+    redirect_to donate_thanks_path
+
+    # request.xhr?
+
+    # respond_to do |format|
+    #   format.json do
+    #   end
+    #   format.all do
+    #   end
+    # end
+
   end
 
   def thanks
