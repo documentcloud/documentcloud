9131b5ab9ebe0470d1e86d467fd692c0c2b7f102
Justin Reese <justin@justinreese.com>
2016-12-05 12:29:10 -0600

    Disarm hashrockets in flash-to-Bootstrap helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,10 +16,10 @@ module ApplicationHelper
 
   def bootstrap_alert_class_for(flash_type)
     {
-      :success => 'alert-success',
-      :error   => 'alert-danger',
-      :alert   => 'alert-warning',
-      :notice  => 'alert-info'
+      success: 'alert-success',
+      error:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-info',
     }[flash_type.to_sym] || flash_type.to_s
   end
 
