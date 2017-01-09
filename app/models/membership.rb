5380a7c622d82c8bc9715c67b91c281406e5d95e
Justin Reese <justin@justinreese.com>
2017-01-09 17:05:56 -0600

    Clean up Membership model syntax

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,24 +2,24 @@ class Membership < ActiveRecord::Base
   include DC::Access
   include DC::Roles
 
-  validates :role,       :inclusion=>{ :in => ROLES }
-  validates :account_id, :uniqueness=> { :scope => :organization_id }
-
   belongs_to :organization
   belongs_to :account
 
-  scope :real,    ->{ where( ["memberships.role in (?)", REAL_ROLES] ) }
-  scope :default, ->{ where(:default=>true) }
+  validates :role,       inclusion:  { in: ROLES }
+  validates :account_id, uniqueness: { scope: :organization_id }
+
+  scope :real,    -> { where(["memberships.role in (?)", REAL_ROLES]) }
+  scope :default, -> { where(default: true) }
 
   def real?
     REAL_ROLES.include?(role)
   end
 
-  def canonical( options = {} )
+  def canonical(options={})
     attrs = self.attributes
-    attrs['account']           = account.canonical(options)      if options[:include_account]
-    attrs['organization']      = organization.canonical(options) if options[:include_organization]
-    attrs['organization_name'] = organization.name               if options[:include_organization_name]
+    attrs[:account]           = account.canonical(options)      if options[:include_account]
+    attrs[:organization]      = organization.canonical(options) if options[:include_organization]
+    attrs[:organization_name] = organization.name               if options[:include_organization_name]
     attrs
   end
   
