b22c8c37879e93bc3ffcad12f82df6552419afc6
Ted Han <ted@knowtheory.net>
2017-07-01 18:11:02 -0700

    Just remove it right now.

--- app/actions/update_admin_dashboard_data.rb
+++ app/actions/update_admin_dashboard_data.rb
@@ -1,5 +1,4 @@
 require File.dirname(__FILE__) + '/support/setup'
-require 'dc/aws'
 
 class UpdateAdminDashboardData < CloudCrowd::Action
 
@@ -22,7 +21,7 @@ class UpdateAdminDashboardData < CloudCrowd::Action
           weekly_hits_on_searches:       keys_to_timestamps(DC::Statistics.weekly_hits_on_searches),
           total_pages:                   DC::Statistics.total_pages,
 
-          instances:                     DC::AWS.new.describe_instances,
+          instances:                     [],
           remote_url_hits_last_week:     DC::Statistics.remote_url_hits_last_week,
           remote_url_hits_all_time:      DC::Statistics.remote_url_hits_all_time,
           count_organizations_embedding: DC::Statistics.count_organizations_embedding,
