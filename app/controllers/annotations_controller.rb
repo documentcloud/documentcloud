7ede50d2fa31932e9301f346cc7794b2fd2ff1b7
Ted Han <ted@knowtheory.net>
2018-05-25 19:25:04 -0700

    No analytics on embedded pages.

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -43,6 +43,7 @@ class AnnotationsController < ApplicationController
           @exclude_analytics = true
           render template: 'annotations/show_embedded', layout: 'new'
         else
+          @include_analytics = true
           make_oembeddable(current_annotation) unless current_annotation.section_note?
           set_minimal_nav text:    'Read the full document',
                           xs_text: 'Full document',
